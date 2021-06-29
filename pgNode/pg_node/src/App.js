9d69b57c452f96647397d383fa38a19c9eda405a
HeathChang <jannhyunsoo@gmail.com>
2021-06-29 17:39:31 +0900

    210629_react_memberAdd

--- pgNode/pg_node/src/App.js
+++ pgNode/pg_node/src/App.js
@@ -1,19 +1,14 @@
 //main 자바스크립트 관리 (body 태그)
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Member from './components/Member';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World :) 
-        </p>
-
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <Member/>
+    )
+    }
 }
 
 export default App;
---
+++ pgNode/pg_node/src/components/Member.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Member extends React.Component { 
+  render(){
+    return(
+    <div>
+      <h2> 홍길동</h2>
+      <p>939044</p>
+      <p>남자</p>
+      <p>대학생</p>
+    </div>
+    )
+  }
+}
+
+export default Member;
