0a452ddd22bb2600e542a871bf9cf77e359b906a
HeathChang <jannhyunsoo@gmail.com>
2021-06-29 18:15:08 +0900

    210629_usingMap

--- pgNode/pg_node/src/App.js
+++ pgNode/pg_node/src/App.js
@@ -3,10 +3,40 @@ import React, {Component} from 'react';
 import './App.css';
 import Member from './components/Member';
 
+const member=[{
+  'name':'홍길동1',
+  'birthday':'941230209',
+  'gender':'남자',
+  'job':'대학생'
+},{
+  'name':'홍동2',
+  'birthday':'9209',
+  'gender':'여자',
+  'job':'울랄라'
+},{
+  'name':'길동3',
+  'birthday':'941230209',
+  'gender':'자자',
+  'job':'드래곤볼'
+}]
+
 class App extends Component {
   render() {
     return(
-      <Member/>
+      <div>
+        {member.map(index=>{
+          return (
+            <Member
+            key={index.name} 
+            name={index.name}
+            birthday={index.birthday}
+            gender={index.gender}
+            job={index.job}
+            /> 
+          )
+        }) 
+        }
+      </div>
     )
     }
 }
--- pgNode/pg_node/src/components/Member.js
+++ pgNode/pg_node/src/components/Member.js
@@ -4,13 +4,32 @@ class Member extends React.Component {
   render(){
     return(
     <div>
-      <h2> 홍길동</h2>
-      <p>939044</p>
-      <p>남자</p>
-      <p>대학생</p>
+        <MemberProfile name={this.props.name} birthday={this.props.birthday}/>
+        <MemberInfo gender={this.props.gender} job={this.props.job}/>
     </div>
     )
   }
 }
+class MemberProfile extends React.Component{
+  render(){
+    return(
+    <div>
+      <h2>{this.props.name}</h2>
+      <p>{this.props.birthday}</p>
+    </div>
+    )
+  }
+}
+class MemberInfo extends React.Component{
+  render(){
+    return(
+    <div>
+      <p>{this.props.gender}</p>
+      <p>{this.props.job}</p>
+    </div>
+    )
+  }
+}
+
 
 export default Member;
