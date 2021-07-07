ff1d4a9bd8facf25c412747e9126b19a9fe329f4
HeathChang <jannhyunsoo@gmail.com>
2021-07-07 22:22:32 +0900

    210707_react_login_1

--- pgNode/pg_node/client/src/pages/member/LoginForm.js
+++ pgNode/pg_node/client/src/pages/member/LoginForm.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../../css/Login.css';
 import {Link} from 'react-router-dom';
 
 
 const LoginForm = () => {
+
+    const [usernameReg,setUsernameReg] = useState("");
+    const [passwordReg,setPasswordReg] = useState("");
+
+
+
+
+
+
+
+
     return (
     <>  
         <script src="../../JS/main.js"/>
@@ -12,10 +23,14 @@ const LoginForm = () => {
             <a href="${request.getContextPath()}/app"> </a>
             <p>" 자신을 알아야 평화를 찾을 수 있는 법 "</p>
             <div class="idForm">
-                <input type="text" name="mbrId" id="mbrId" placeholder="ID"/>
+                <input type="text" name="mbrId" id="mbrId" placeholder="ID" onChange={(e)=> {
+                    setUsernameReg(e.target.value);    
+                }}/>
             </div>
             <div class="pwForm">
-                <input type="text" name="mbrPw" id="mbrPw" placeholder="PassWord"/>
+                <input type="text" name="mbrPw" id="mbrPw" placeholder="PassWord" onChange={(e)=> {
+                    setPasswordReg(e.target.value);    
+                }}/>
             </div>
             <div class="button">
                 <button type="submit" name="login" id="login">L O G I N</button>
--- pgNode/pg_node/client/src/pages/member/RegisterPage.js
+++ pgNode/pg_node/client/src/pages/member/RegisterPage.js
@@ -1,15 +1,44 @@
-import React from 'react';
+//JS part
+import React,{useState} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+
+//design part
 import Header from '../common/header';
 import Footer from '../common/footer';
 import '../../css/MemberAdd.css';
-import {Link} from 'react-router-dom';
+
 
 const RegisterPage = () => {
+    //set
+    const [usernameReg,setUsernameReg] = useState("");
+    const [passwordReg,setPasswordReg] = useState("");
+    const [password2Reg,setPassword2Reg] = useState("");
+    const [nickNameReg,setNicknameReg] = useState("");
+    const [emailReg,setEmailReg] = useState("");
+    const [tagReg,setTagReg] = useState("");
+
+    //fn
+    const register=()=>{
+        axios.post("http://http://localhost:3000/register",
+        {
+            mbrId:usernameReg,
+            mbrPw:passwordReg,
+            mbrName:nickNameReg,
+            mbrEmail:emailReg,
+            mbrGenre:tagReg
+        }).then((response)=>{
+            console.log(response);
+            window.location = "/";
+        })
+    }//end register
+
+
     return (
     <>
         <body>
         <div>
-                <form action="memberAdd" method="post" className="addForm" id="addForm">
+                <form method="post" className="addForm" id="addForm">
                 <Link to="/"><img className="logo" src={require('../../Image/logo.png').default} /></Link> 
                     <h2>회원가입</h2>
 
@@ -18,13 +47,17 @@ const RegisterPage = () => {
                             <span className="title">아이디 *
                             <button id="idchk" className="idchk" onclick="idCheck(); return false;">중복체크</button>
                             </span> 
-                            <input type="text" className="content" name="mbrId" autoFocus/>
+                            <input type="text" className="content" name="mbrId" autoFocus onChange={(e)=> {
+                            setUsernameReg(e.target.value);    
+                            }}/>
                             <span id="result"/>
                         </div>
 
                         <div className="row">
                             <span className="title">비밀번호 *</span> 
-                            <input type="text" className="content" name="mbrPw" autocomplete="off" placeholder=" 비밀번호는 6자리 이상이어야 합니다."/>
+                            <input type="text" className="content" name="mbrPw" autocomplete="off" placeholder=" 비밀번호는 6자리 이상이어야 합니다." onChange={(e)=> {
+                            setPasswordReg(e.target.value);    
+                            }}/>
                         </div>
 
                         <div className="row" styles="display: inline;">
@@ -32,34 +65,42 @@ const RegisterPage = () => {
                             비밀번호 확인 * 
                             <span id="pwcheck"/>
                             </span> 
-                            <input type="text" className="content" name="mbrPw2" id="mbrPw2"/>
+                            <input type="text" className="content" name="mbrPw2" id="mbrPw2" onChange={(e)=> {
+                            setPassword2Reg(e.target.value);    
+                            }}/>
                         </div>
 
                         <div className="row">
                             <span className="title">닉네임 *
                             <button id="nickchk" className="nickchk" onclick="nickCheck(); return false;">중복체크</button>
                             </span> 
-                            <input type="text" className="content" name="mbrName" id="mbrName"/>
+                            <input type="text" className="content" name="mbrName" id="mbrName" onChange={(e)=> {
+                            setNicknameReg(e.target.value);    
+                            }}/>
                         </div>
 
                         <div className="row">
                             <span className="title">이메일 *
                             <button id="emailchk" className="emailchk" onclick="emailCheck(); return false;">중복체크</button>
                             </span> 
-                            <input type="email" className="content" name="mbrEmail" id="mbrEmail"/>
+                            <input type="email" className="content" name="mbrEmail" id="mbrEmail" onChange={(e)=> {
+                            setEmailReg(e.target.value);    
+                            }}/>
                         </div>
 
                         <div className="row">
                             <span className="title">태그 *
                             <button id="mbrGenre" className="mbrGenre" onclick="tag(); return false;">Tag</button>
                             </span> 
-                            <input type="text" className="content" id="mbrName"/>
+                            <input type="text" className="content" id="mbrName" onChange={(e)=> {
+                            setTagReg(e.target.value);    
+                            }}/>
 
                         </div>
 
 
                         <div className="button">
-                            <input type="submit" value="제 출"/>
+                            <button onClick={register}> 등록</button>
                         </div>
 
                     </div>
