14263f8d8894d93d789c4990fc86f701df5283f9
HeathChang <jannhyunsoo@gmail.com>
2021-07-14 02:21:42 +0900

    210714_coding_practice_1

--- coding_practice/java_coding_practice/21_07/src/java_coding/Practice08_210714.java
+++ coding_practice/java_coding_practice/21_07/src/java_coding/Practice08_210714.java
@@ -13,13 +13,53 @@ public class Practice08_210714 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("배열의 크기 입력: ");
-		int arrSize=Integer.parseInt(bf.readLine());
-		Integer [] arr = new Integer[arrSize];
+		int [] arr = new int[Integer.parseInt(bf.readLine())];
+		
+		
+		System.out.println("사용할 수의 범위 (1~n): ");
+		int n=Integer.parseInt(bf.readLine());
 		
 		System.out.print("원 배열: ");
-		for (int i = 0; i < arrSize ; i++) {
-			arr[i] =(int) (Math.random()*10);
+		for (int i = 0; i < arr.length ; i++) {
+			arr[i] =(int) (Math.random()*n);
 			System.out.print(arr[i]);
+		}
+		System.out.println();
+		//////////////////
+		int [] counting = new int[n]; // 1~n까지 범위를 가진 배열
+		int [] result = new int [arr.length]; // 배열 정렬 결과
+		
+		
+		//과정 1: arr의 value값을 index로 하는 counting [] 값 1 증가
+		for (int i = 0; i < arr.length; i++) {
+			counting[arr[i]]+=1;
+		}
+		
+		//과정 2: 누적 합 더해주기
+		for (int i = 1; i < counting.length; i++) {
+			counting[i]= counting[i]+ counting[i-1];
+		}
+		
+		//과정 3: i번째를 idx로 하는 counting 배열 1감소후, counting 배열의 값을 인덱스로해 result에 저장
+		for (int i = arr.length-1; i >=0; i--) {
+			int value = arr[i];
+			counting[value]-=1;
+			result[counting[value]] = value;
+		}
+		
+		System.out.println("array[]");
+		for(int i = 0; i < arr.length; i++) {
+			if(i % 10 == 0) System.out.println();
+			System.out.print(arr[i] + "\t");
+		}
+		System.out.println("\n\n");
+
+		
+		// 정렬된 배열
+		System.out.println("result[]");
+		for(int i = 0; i < result.length; i++) {
+			if(i % 10 == 0) System.out.println();
+			System.out.print(result[i] + "\t");
 		}
 		
 	
