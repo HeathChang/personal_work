ec24a5c07f95d95cbe741b4b21c620b3aeed1ca6
HeathChang <jannhyunsoo@gmail.com>
2021-07-18 20:04:30 +0900

    210718_coding_algo_2

--- coding_practice/java_coding_practice/21_07/src/java_coding/Practice05_210713_upperCase.java
+++ coding_practice/java_coding_practice/21_07/src/java_coding/Practice05_210713_upperCase.java
@@ -4,24 +4,35 @@ import java.util.Scanner;
 
 //지정한 숫자만 대문자로 바꾸기
 public class Practice05_210713_upperCase {
-	int fiboData[100] = {0,};
-	public static int fibonacci(int nth) {
-		  fibodata[0] = 0;
-		  fiboData[1] = 1;
-		  for (int i=2; i<=nth; i++)
-		    fiboData[i] = fiboData[i - 1] + fiboData[i - 2];
-		  return fiboData[nth];
-		
-	}
+	    public int solution(int left, int right) {
+	        int answer = 0;
+	        for(int i = left ; i<=right ; i++){
+		       int process = 0;
+	           for(int j = 1 ; j <= i ; j++){ 
+	               if(i % j == 0 ){
+	                process ++;
+	               } 
+	           } //end j for
+	            
+	            if(process % 2 == 0){
+	                answer= answer + i;
+	                System.out.println("더하기: "+answer);
+
+	            }else {
+	                answer= answer - i;
+	                System.out.println("빼기: "+answer);
+	            }
+	         
+	        }
+	        
+	        return answer;
+	    }
+	
 
 	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		System.out.println("몇번째 숫자를 알고 싶으신가요? : ");
-		int nth = scan.nextInt();
-		int result = fibonacci(nth);
-		System.out.println(nth + " 번째 피보나치 수열 값은 " + result);
-
+		Practice05_210713_upperCase test = new Practice05_210713_upperCase();
+		int ans = test.solution(13,17);
+		System.out.println("답은: "+ans);
 	}
 
 }
