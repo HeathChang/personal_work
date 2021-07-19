ad2cd1238d6e46c0f82878fd33284e8df75ccbf8
HeathChang <jannhyunsoo@gmail.com>
2021-07-19 23:34:41 +0900

    210719_coding_algo_3

--- coding_practice/java_coding_practice/21_07/src/java_coding/Practice17_210719_middleCharacter.java
+++ coding_practice/java_coding_practice/21_07/src/java_coding/Practice17_210719_middleCharacter.java
@@ -3,7 +3,7 @@ package java_coding;
 import java.util.*;
 
 //가운데 글자 뽑아내기: https://programmers.co.kr/learn/courses/30/lessons/12903
-public class AA00_test {
+public class Practice17_210719_middleCharacter {
 	public String solution(String s) {
 		String answer = "";
 		String[] strArray = s.split("");
@@ -20,7 +20,7 @@ public class AA00_test {
 
 	public static void main(String[] args) {
 		String a = "abcde";
-		AA00_test test = new AA00_test();
+		Practice17_210719_middleCharacter test = new Practice17_210719_middleCharacter();
 		String ans = test.solution(a);
 		System.out.println("답안: " + ans);
 	}
