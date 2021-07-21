81147a448828c52e45b3d0b5bd0e872b3d4a822e
HeathChang <jannhyunsoo@gmail.com>
2021-07-21 20:41:43 +0900

    210721_coding_algo

--- coding_practice/java_coding_practice/21_07/src/java_coding/AA00_test.java
+++ coding_practice/java_coding_practice/21_07/src/java_coding/AA00_test.java
@@ -4,34 +4,26 @@ import java.util.*;
 
 //가장 작은수 제거:  https://programmers.co.kr/learn/courses/30/lessons/12935 
 public class AA00_test {
-	public int[] solution(int[] arr) {
-		if(arr.length <= 1) {
-			int [] answer= {-1};
-			return answer;
-		}
-		int small = arr[0];
-		for (int i = 1; i < arr.length; i++) {
-			small = (small<arr[i])?small:arr[i];
-		}		
-		
-		int[] answer = new int [arr.length-1];
-		int index = 0;
-		for (int j = 0; j < arr.length; j++) {
-			if(arr[j]==small) {
-				continue;
-			}
-			answer[index++]=arr[j];
-		}
-		return answer;
+    public int solution(String numbers) {
+        int answer = 0;
+        String[] arr=numbers.split("");
+        int [] arr2= new int [arr.length];
+        for (int i = 0; i < arr.length; i++) {
+        	arr2[i]=Integer.parseInt(arr[i]);
+        }
+        
+        
+        
+        
+        
+        return answer;
+    
 	}
 
 	public static void main(String[] args) {
-		int [] seoul = {1,2,3,4,5};
+		String seoul = "011";
 		AA00_test test = new AA00_test();
-		int [] ans = test.solution(seoul);
+		int ans = test.solution(seoul);
 		System.out.println("답안: " + ans);
-		for (int i = 0; i < ans.length; i++) {
-			System.out.println(ans[i]);
-		}
 	}
 } // end
