b6aef9fa711b167375234d92d5aea86fea3d2ff6
HeathChang <jannhyunsoo@gmail.com>
2021-07-26 16:15:10 +0900

    210726_nodeJS_review_2

--- NodeJS/00_Intro/009_Text_Create/main.js
+++ NodeJS/00_Intro/009_Text_Create/main.js
@@ -1,4 +1,4 @@
-//글목록 출력하기
+//글 생성하여, 보내기: f12를 누를시, NETWORK>>에서 FORM DATA 확인 가능
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
@@ -7,16 +7,16 @@ function templateHTML(title, list, body) {
   return `
   <!doctype html>
   <html>
-  <head>
-    <title>WEB1 - ${title} </title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB2</a></h1>
-    ${list}
-    <h2>${title}</h2>
-    <p>${body}</p>
-  </body>
+    <head>
+      <title>WEB1 - ${title} </title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      <a href='/create'>create</a>
+      <p>${body}</p>
+    </body>
   </html>
   `;
 }
@@ -72,6 +72,24 @@ var app = http.createServer(function (request, response) {
         );
       });
     }
+  } else if (pathname == '/create') {
+    fs.readdir("./data", function (err, fileList) {
+      var title = 'WEB_CREATE';
+      var list = templateList(fileList);
+      var template = templateHTML(title, list, `
+        <form action = "http://localhost:3000/create_process" method= "post">
+          <p> <input type ="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name ="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type ="submit">
+          </p>
+        </form>
+      `);
+      response.writeHead(200);
+      response.end(template);
+    });
   } else {
     //4. 루트가 아닐 때의 처리 (404)
     console.log(url.parse(_url, true));
