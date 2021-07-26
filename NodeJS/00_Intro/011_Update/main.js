11ee6d7b769e2c6c33d2c6f86e74cec494a815ef
HeathChang <jannhyunsoo@gmail.com>
2021-07-26 23:16:15 +0900

    210726_nodeJS_review_4

--- NodeJS/00_Intro/011_Update/main.js
+++ NodeJS/00_Intro/011_Update/main.js
@@ -1,11 +1,10 @@
-//POST 방식으로 전달한 데이터를 가져와 쳐리하는 코드 && 파일 저장 및 리 다이렉션
+//클릭시 update로 이동하고 수정할 정보를 전송
 var http = require("http");
 var qs = require('querystring');
 var fs = require("fs");
 var url = require("url");
-const path = require("path/posix");
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `
   <!doctype html>
   <html>
@@ -16,8 +15,8 @@ function templateHTML(title, list, body) {
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
-      <a href='/create'>create</a>
-      <p>${body}</p>
+      ${control}
+      ${body}
     </body>
   </html>
   `;
@@ -49,7 +48,8 @@ var app = http.createServer(function (request, response) {
         var template = templateHTML(
           title,
           list,
-          `<h2>${title}</h2><p>${description}</p>`
+          `<h2>${title}</h2><p>${description}</p>`,
+          `<a href='/create'>create</a>`
         );
         response.writeHead(200);
         response.end(template);
@@ -66,7 +66,9 @@ var app = http.createServer(function (request, response) {
             var template = templateHTML(
               title,
               list,
-              `<h2>${title}</h2><p>${description}</p>`
+              `<h2>${title}</h2><p>${description}</p>`,
+              `<a href='/create'>create</a>
+              <a href='/update?id=${title}'>update</a>`
             );
             response.writeHead(200);
             response.end(template);
@@ -74,6 +76,7 @@ var app = http.createServer(function (request, response) {
         );
       });
     }
+    //>>>>>생성
   } else if (pathname == '/create') {
     fs.readdir("./data", function (err, fileList) {
       var title = 'WEB_CREATE';
@@ -88,7 +91,7 @@ var app = http.createServer(function (request, response) {
             <input type ="submit">
           </p>
         </form>
-      `);
+      `, '');
       response.writeHead(200);
       response.end(template);
     });
@@ -114,6 +117,56 @@ var app = http.createServer(function (request, response) {
         response.writeHead(302, { Location: `/?id=${title}` });
         response.end();
       })
+    });
+
+    //>>>>>수정  
+  } else if (pathname == '/update') {
+    fs.readdir("./data", function (err, fileList) {
+      fs.readFile(
+        `data/${queryData.id}`,
+        "utf8",
+        function (err, description) {
+          var title = queryData.id;
+          var list = templateList(fileList);
+          var template = templateHTML(
+            title,
+            list,
+            `<form action = "/update_process" method= "post">
+              <input type = "hidden" name = "id" value = "${title}">
+              <p> <input type ="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name ="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type ="submit">
+              </p>
+            </form>`,
+            `<a href='/create'>create</a> <a href='/update?id=${title}'>update</a>`
+
+          );
+          response.writeHead(200);
+          response.end(template);
+        }
+      );
+    });
+  } else if (pathname == '/update_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body += data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      //rename: 1번째: 기존파일명 & 2번째: 수정파일명 & 3번째 에러발생시, 콜백
+      fs.rename(`data/${id}`, `data/${title}`, function (err) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end();
+        })
+      })
+
     });
   } else {
     //4. 루트가 아닐 때의 처리 (404)
