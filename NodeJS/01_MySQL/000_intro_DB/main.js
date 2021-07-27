e53053d267cc9856ce6d98a10fbdeb12c8ef0372
HeathChang <jannhyunsoo@gmail.com>
2021-07-27 23:06:46 +0900

    210727_NodeJS_review_8

--- NodeJS/01_MySQL/000_intro_DB/main.js
+++ NodeJS/01_MySQL/000_intro_DB/main.js
@@ -31,33 +31,32 @@ var app = http.createServer(function (request, response) {
         response.end(html);
       });
     } else {
-      fs.readdir("./data", function (err, fileList) {
-        fs.readFile(
-          `data/${queryData.id}`,
-          "utf8",
-          function (err, description) {
-            var title = queryData.id;
-            var list = template.list(fileList);
-            var html = template.HTML(
-              title,
-              list,
-              `<h2>${title}</h2><p>${description}</p>`,
-              `<a href='/create'>create</a>
-              <a href='/update?id=${title}'>update</a>
-              <form action= "delete_process" method = "post" onsubmit= "return confirm('ARE YOU SURE?')">
-                <input type ="hidden" name="id" value="${title}">
-                <!-- 주의: placeholder 사용 시, value의 값이 지정되어 있지 않음--!>
-                <input type ="submit" value= "delete">
-              </form>
-              `
-            );
-            response.writeHead(200);
-            response.end(html);
-          }
+      //3. 쿼리 스트링
+      db.query(`SELECT * FROM topic where id = ?`, [queryData.id], function (error, topic) {
+        //공격의도가 있는 코드를 알아서 걸러줌
+        if (error) {
+          throw error;
+        }
+        var title = topic[0].title;
+        var description = topic[0].description;
+        var list = template.list(topic);
+        var html = template.HTML(
+          title,
+          list,
+          `<h2>${title}</h2><p>${description}</p>`,
+          `<a href='/create'>create</a>
+          <a href='/update?id=${title}'>update</a>
+          <form action= "delete_process" method = "post" onsubmit= "return confirm('ARE YOU SURE?')">
+            <input type ="hidden" name="id" value="${title}">
+            <input type ="submit" value= "delete">
+          </form>
+          `
         );
+        response.writeHead(200);
+        response.end(html);
       });
     }
-    //>>>>>생성
+    /////생성 >>>> 새로운 내용
   } else if (pathname == '/create') {
     fs.readdir("./data", function (err, fileList) {
       var title = 'WEB_CREATE';
@@ -103,8 +102,9 @@ var app = http.createServer(function (request, response) {
     //>>>>>수정  
   } else if (pathname == '/update') {
     fs.readdir("./data", function (err, fileList) {
+      var filteredId = path.parse(queryData.id).base;
       fs.readFile(
-        `data/${queryData.id}`,
+        `data/${filteredId}`,
         "utf8",
         function (err, description) {
           var title = queryData.id;
@@ -123,7 +123,6 @@ var app = http.createServer(function (request, response) {
               </p>
             </form>`,
             `<a href='/create'>create</a> <a href='/update?id=${title}'>update</a>`
-
           );
           response.writeHead(200);
           response.end(html);
@@ -157,8 +156,9 @@ var app = http.createServer(function (request, response) {
     request.on('end', function () {
       var post = qs.parse(body);
       var id = post.id;
-      //unlink → 삭제: 1번째: 삭제 파일 && 2번째: 삭제 완료 후 처리할 내용 콜백  
-      fs.unlink(`data/${id}`, function () {
+      //unlink → 삭제: 1번째: 삭제 파일 && 2번째: 삭제 완료 후 처리할 내용 콜백 
+      var filteredId = path.parse(id).base;
+      fs.unlink(`data/${filteredId}`, function () {
         response.writeHead(302, { Location: '/' });
         response.end();
       })
