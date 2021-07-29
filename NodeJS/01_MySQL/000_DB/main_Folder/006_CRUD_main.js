add389029aa6806b456d534a62c3ad362ce15599
HeathChang <jannhyunsoo@gmail.com>
2021-07-29 18:26:07 +0900

    210729_introExpress_4

---
+++ NodeJS/01_MySQL/000_DB/main_Folder/006_CRUD_main.js
@@ -0,0 +1,50 @@
+//저자 생성 기능 (create)
+var http = require("http");
+var qs = require('querystring');
+var url = require("url");
+//export
+var template = require('./lib/template');
+var topic = require('./lib/topic');
+var author = require('./lib/author');
+//DB 설정
+var db = require('./database/database');
+
+
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  if (pathname == "/") {
+    if (queryData.id == undefined) {
+      topic.home(request, response);
+    } else {
+      //쿼리 스트링
+      topic.page(request, response);
+    }
+  } else if (pathname == '/create') {
+    topic.create(request, response);
+  } else if (pathname == '/create_process') {
+    topic.create_process(request, response);
+  } else if (pathname == '/update') {
+    topic.update(request, response);
+  } else if (pathname == '/update_process') {
+    topic.update_process(request, response);
+  } else if (pathname == '/delete_process') {
+    topic.delete(request, response);
+  } else if (pathname == '/author') {
+    author.home(request, response);
+  } else if (pathname == '/author/create_process') {
+    author.create_process(request, response);
+  } else if (pathname == '/author/update') {
+    author.update(request, response);
+  } else if (pathname == '/author/update_process') {
+    author.update_process(request, response);
+  } else if (pathname == '/author/delete_process') {
+    author.delete_process(request, response);
+  } else {
+    console.log(url.parse(_url, true));
+    response.writeHead(404);
+    response.end("Not Found");
+  }
+});
+app.listen(3000);
