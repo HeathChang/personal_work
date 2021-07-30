9b7ab012d0bea25f06af96b85707fa4b99d36ff6
HeathChang <jannhyunsoo@gmail.com>
2021-07-30 22:46:55 +0900

    210730_introExpress_4

--- NodeJS/02_Express/001_intro_Express/main.js
+++ NodeJS/02_Express/001_intro_Express/main.js
@@ -7,6 +7,7 @@ var template = require('./lib/template');
 
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+const { response } = require('express');
 
 app.get('/', function (request, response) {
   fs.readdir("./data", function (err, fileList) {
@@ -34,14 +35,17 @@ app.get('/page/:pageId', function (request, response) {
         allowedTags: ['h1']
       });
       var list = template.list(filelist);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-                  <a href="/update?id=${sanitizedTitle}">update</a>
-                  <form action="delete_process" method="post">
-                      <input type="hidden" name="id" value="${sanitizedTitle}">
-                      <input type="submit" value="delete">
-                  </form>`
+      var html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
+        `<a href='/create'>create</a>
+        <a href='/update/${sanitizedTitle}'>update</a>
+        <form action= "delete_process" method = "post" onsubmit= "return confirm('ARE YOU SURE?')">
+          <input type ="hidden" name="id" value="${sanitizedTitle}">
+          <input type ="submit" value= "delete">
+        </form>
+        `
       );
       response.send(html);
     });
@@ -81,6 +85,52 @@ app.post('/create_process', (request, response) => {
     fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
       response.writeHead(302, { Location: `/page/${title}` });
       response.end();
+    })
+  })
+})
+
+app.get('/update/:pageId', (request, response) => {
+  fs.readdir('./data', function (error, filelist) {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+      var title = request.params.pageId;
+      var list = template.list(filelist);
+      var html = template.HTML(title, list,
+        `
+            <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                <p>
+                    <textarea name="description"
+                        placeholder="description">${description}</textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>
+            `,
+        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+      );
+      response.send(html);
+    });
+  });
+})
+
+app.post('/update_process', (request, response) => {
+  var body = '';
+  request.on('data', function (data) {
+    body += data;
+  });
+  request.on('end', function () {
+    var post = qs.parse(body);
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, description, function (err1) {
+      fs.writeFile(`data/${title}`, description, 'utf8', function (err2) {
+        response.writeHead(302, { Location: `/page/${title}` });
+        response.end();
+      })
     })
   })
 })
