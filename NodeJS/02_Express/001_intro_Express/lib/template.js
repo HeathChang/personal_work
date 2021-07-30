c2f92b611fff32c74803e0b48c3a99aa1257d0b6
HeathChang <jannhyunsoo@gmail.com>
2021-07-30 21:22:36 +0900

    210730_introExpress_2

--- NodeJS/02_Express/001_intro_Express/lib/template.js
+++ NodeJS/02_Express/001_intro_Express/lib/template.js
@@ -1,62 +1,29 @@
-module.exports = {
+var template = {
   HTML: function (title, list, body, control) {
     return `
-      <!doctype html>
-      <html>
-          <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-          </head>
-          <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              ${control}
-              ${body}
-          </body>
-      </html>
-      `;
-  }, list: function (topics) {
-    var list = '<ul>';
-    var i = 0;
-    while (i < topics.length) {
-      list = list + `<li><a href="/?id=${topics[i].id}">${topics[i].title}</a></li>`;
-      i = i + 1;
-    }
-    list = list + '</ul>';
-    return list;
-  }, authorSelect: function (authors, author_id) {
-    var tag = '';
-    var i = 0;
-    while (i < authors.length) {
-      var selected = '';
-      if (authors[i].id === author_id) {
-        selected = ' selected';
-      }
-      tag += `<option value="${authors[i].id}"${selected}>${authors[i].name}</option>`;
-      i++;
-    }
-    return `
-          <select name="author">
-              ${tag}
-          </select>
-      `
+    <!doctype html>
+    <html>
+      <head>
+        <title>WEB1 - ${title} </title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        ${control}
+        ${body}
+      </body>
+    </html>
+    `;
   },
-  authorTable: function (authors) {
-    var tag = '<table>';
-    for (var i = 0; i < authors.length; i++) {
-      tag += `<tr>
-                  <td>${authors[i].name}</td>
-                  <td>${authors[i].profile}</td>
-                  <td><a href= "/author/update?id=${authors[i].id}">update</a></td>
-                  <td>
-                    <form action ="/author/delete_process" method = "POST">
-                      <input type = "hidden" name ="id" value = "${authors[i].id}">
-                      <input type = "submit" value = "delete">
-                    </form>
-                  </td>
-              </tr>`
+  list: function (fileList) {
+    var list = "<ul>";
+    for (var i = 0; i < fileList.length; i++) {
+      list += `<li><a href="/page/${fileList[i]}">${fileList[i]}</a></li>`;
     }
-    tag += '</table>';
-    return tag;
+    list += "</ul>";
+    return list;
   }
 }
+
+module.exports = template;
--- NodeJS/02_Express/001_intro_Express/main.js
+++ NodeJS/02_Express/001_intro_Express/main.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express(); //반환하는 값: Express App에서 제공하는 Application 객체 (get,listen 포함)
 
 var fs = require('fs');
-var template = require('./lib/template_1');
+var template = require('./lib/template');
 
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+
 app.get('/', function (request, response) {
   fs.readdir("./data", function (err, fileList) {
     var title = "Welcome";
@@ -22,31 +23,28 @@ app.get('/', function (request, response) {
   });
 })
 
-app.get('/page/:pageId', (request, response) => {
-  fs.readFile(
-    `data/`,
-    "utf8",
-    function (err, description) {
-      var title = queryData.id;
+app.get('/page/:pageId', function (request, response) {
+  fs.readdir('./data', function (error, filelist) {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+      var title = request.params.pageId;
       var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description);
-      var list = template.list(fileList);
-      var html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
-        `<a href='/create'>create</a>
-        <a href='/update?id=${sanitizedTitle}'>update</a>
-        <form action= "delete_process" method = "post" onsubmit= "return confirm('ARE YOU SURE?')">
-          <input type ="hidden" name="id" value="${sanitizedTitle}">
-          <!-- 주의: placeholder 사용 시, value의 값이 지정되어 있지 않음--!>
-          <input type ="submit" value= "delete">
-        </form>
-        `
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+                  <a href="/update?id=${sanitizedTitle}">update</a>
+                  <form action="delete_process" method="post">
+                      <input type="hidden" name="id" value="${sanitizedTitle}">
+                      <input type="submit" value="delete">
+                  </form>`
       );
       response.send(html);
-    }
-  );
+    });
+  });
 });
 
 app.listen(3000, function () {
