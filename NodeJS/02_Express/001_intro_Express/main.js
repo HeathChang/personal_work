b39b13e32c83bc53263286cea676e0c5ebf09834
HeathChang <jannhyunsoo@gmail.com>
2021-07-31 21:24:48 +0900

    210731_introExpress_middleWare_1

--- NodeJS/02_Express/001_intro_Express/main.js
+++ NodeJS/02_Express/001_intro_Express/main.js
@@ -8,6 +8,11 @@ var template = require('./lib/template');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 const { response } = require('express');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+//해당 미들웨어가 실행되고 결과를 리턴. (→main.js 실행마다 미들웨어 실행 )
+//사용자가 post 방식으로 전송한 데이터를  _process에서의 동작을 통해 결과를 전달 
 
 app.get('/', function (request, response) {
   fs.readdir("./data", function (err, fileList) {
@@ -72,20 +77,13 @@ app.get('/create', (request, response) => {
 })
 
 app.post('/create_process', (request, response) => {
-  // /create에서 post로 전달되기때문에 post로 설정
-  var body = '';
-  request.on('data', function (data) {
-    body += data;
-  });
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-    console.log("title: ", title, "\n", "des: ", description);
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      response.writeHead(302, { Location: `/page/${title}` });
-      response.end();
-    })
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  console.log("title: ", title, "\n", "des: ", description);
+  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+    response.writeHead(302, { Location: `/page/${title}` });
+    response.end();
   })
 })
 
@@ -117,35 +115,23 @@ app.get('/update/:pageId', (request, response) => {
 })
 
 app.post('/update_process', (request, response) => {
-  var body = '';
-  request.on('data', function (data) {
-    body += data;
-  });
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, description, function (err1) {
-      fs.writeFile(`data/${title}`, description, 'utf8', function (err2) {
-        response.redirect(`/page/${title}`);
-        response.end();
-      })
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, description, function (err1) {
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err2) {
+      response.redirect(`/page/${title}`);
+      response.end();
     })
   })
 })
 app.post('/delete_process', (request, response) => {
-  var body = '';
-  request.on('data', function (data) {
-    body += data;
-  });
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function () {
-      response.redirect('/');
-    })
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function () {
+    response.redirect('/');
   })
 })
 
