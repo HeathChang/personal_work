fe470729925dca6ac44e8b01234d0f1b44ca1719
HeathChang <jannhyunsoo@gmail.com>
2021-08-01 00:34:23 +0900

    210801_introExpress_middleWare_error_5

--- NodeJS/02_Express/001_intro_Express/main.js
+++ NodeJS/02_Express/001_intro_Express/main.js
@@ -5,12 +5,14 @@ var qs = require('querystring');
 
 var fs = require('fs');
 var template = require('./lib/template');
-
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 const { response } = require('express');
 var bodyParser = require('body-parser');
 var compression = require('compression');
+
+var topicRouter = require('./routes/topic');
+//topic 이라는 라우터 적용 정의
 
 app.use(bodyParser.urlencoded({ extended: false }));
 //원하는 요청이 왔을 경우에만 실행되는 미들웨어 (use  → get으로 변경) 
@@ -35,115 +37,7 @@ app.get('/', function (request, response) {
   );
   response.send(html);
 });
-
-app.get('/topic/create', (request, response) => {
-  var title = 'WEB_CREATE';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-      <form action = "http://localhost:3000/topic/create_process" method= "post">
-        <p> <input type ="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name ="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type ="submit">
-        </p>
-      </form>
-    `, '');
-  response.send(html);
-});
-
-app.post('/topic/create_process', (request, response) => {
-  console.log(request.list);
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  console.log("title: ", title, "\n", "des: ", description);
-  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    response.writeHead(302, { Location: `/topic/${title}` });
-    response.end();
-  })
-})
-
-app.get('/topic/update/:pageId', (request, response) => {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-            <form action="/topic/update_process" method="post">
-                <input type="hidden" name="id" value="${title}">
-                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-                <p>
-                    <textarea name="description"
-                        placeholder="description">${description}</textarea>
-                </p>
-                <p>
-                    <input type="submit">
-                </p>
-            </form>
-            `,
-      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-    );
-    response.send(html);
-  });
-});
-
-app.post('/topic/update_process', (request, response) => {
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, description, function (err1) {
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err2) {
-      response.redirect(`/topic/${title}`);
-      response.end();
-    })
-  })
-})
-
-
-
-
-app.post('/topic/delete_process', (request, response) => {
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function () {
-    response.redirect('/');
-  })
-})
-
-
-app.get('/topic/:pageId', function (request, response, next) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    if (err) {
-      next(err); //에러 전달
-    } else {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
-        `<a href='/topic/create'>create</a>
-        <a href='/topic/update/${sanitizedTitle}'>update</a>
-        <form action= "/topic/delete_process" method = "post" onsubmit= "return confirm('ARE YOU SURE?')">
-          <input type ="hidden" name="id" value="${sanitizedTitle}">
-          <input type ="submit" value= "delete">
-        </form>
-        `
-      );
-      response.send(html);
-    }
-  });
-});
+app.use('/topic', topicRouter);
 
 app.use(function (req, res, next) {
   res.status(404).send("Sorry 404 Error.")
