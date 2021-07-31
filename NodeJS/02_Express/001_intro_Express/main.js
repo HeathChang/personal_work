3a54a9478fe57adde604467473b5800822a8950b
HeathChang <jannhyunsoo@gmail.com>
2021-08-01 00:43:05 +0900

    210801_introExpress_middleWare_error_6

--- NodeJS/02_Express/001_intro_Express/main.js
+++ NodeJS/02_Express/001_intro_Express/main.js
@@ -1,18 +1,21 @@
 //p463 라우트 파일 처리, 주소 체계의 변경
+//기본
 const express = require('express');
 const app = express(); //반환하는 값: Express App에서 제공하는 Application 객체 (get,listen 포함)
-var qs = require('querystring');
 
 var fs = require('fs');
-var template = require('./lib/template');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 const { response } = require('express');
 var bodyParser = require('body-parser');
 var compression = require('compression');
+var qs = require('querystring');
 
+//라우터 정의
 var topicRouter = require('./routes/topic');
-//topic 이라는 라우터 적용 정의
+var indexRouter = require('./routes/index');
+//외부 템플릿
+var template = require('./lib/template');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 //원하는 요청이 왔을 경우에만 실행되는 미들웨어 (use  → get으로 변경) 
@@ -24,19 +27,7 @@ app.get('*', function (request, response, next) {
 })
 app.use(compression());
 
-app.get('/', function (request, response) {
-  var title = "Welcome";
-  var description =
-    "This is Main Page. Click list to move to other pages";
-  var list = template.list(request.list);
-  var html = template.HTML(
-    title,
-    list,
-    `<h2>${title}</h2><p>${description}</p>`,
-    `<a href='/topic/create'>create</a>`
-  );
-  response.send(html);
-});
+app.use('/', indexRouter)
 app.use('/topic', topicRouter);
 
 app.use(function (req, res, next) {
