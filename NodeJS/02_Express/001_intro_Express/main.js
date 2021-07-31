a09300433415843dde702aa8b19f4d8770d3eb22
HeathChang <jannhyunsoo@gmail.com>
2021-08-01 00:21:53 +0900

    210801_introExpress_middleWare_error_3

--- NodeJS/02_Express/001_intro_Express/main.js
+++ NodeJS/02_Express/001_intro_Express/main.js
@@ -1,4 +1,4 @@
-//p461 에러 처리하는 방법
+//p463 라우트 파일 처리, 주소 체계의 변경
 const express = require('express');
 const app = express(); //반환하는 값: Express App에서 제공하는 Application 객체 (get,listen 포함)
 var qs = require('querystring');
@@ -31,12 +31,92 @@ app.get('/', function (request, response) {
     title,
     list,
     `<h2>${title}</h2><p>${description}</p>`,
-    `<a href='/create'>create</a>`
+    `<a href='/topic/create'>create</a>`
   );
   response.send(html);
 });
 
-app.get('/page/:pageId', function (request, response, next) {
+app.get('/topic/create', (request, response) => {
+  var title = 'WEB_CREATE';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
+      <form action = "http://localhost:3000/topic/create_process" method= "post">
+        <p> <input type ="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name ="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type ="submit">
+        </p>
+      </form>
+    `, '');
+  response.send(html);
+});
+
+app.post('/topic/create_process', (request, response) => {
+  console.log(request.list);
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  console.log("title: ", title, "\n", "des: ", description);
+  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+    response.writeHead(302, { Location: `/topic/${title}` });
+    response.end();
+  })
+})
+
+app.get('/topic/update/:pageId', (request, response) => {
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+    var title = request.params.pageId;
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+      `
+            <form action="/topic/update_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                <p>
+                    <textarea name="description"
+                        placeholder="description">${description}</textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>
+            `,
+      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+    );
+    response.send(html);
+  });
+});
+
+app.post('/topic/update_process', (request, response) => {
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, description, function (err1) {
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err2) {
+      response.redirect(`/topic/${title}`);
+      response.end();
+    })
+  })
+})
+
+
+
+
+app.post('/topic/delete_process', (request, response) => {
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function () {
+    response.redirect('/');
+  })
+})
+
+
+app.get('/topic/:pageId', function (request, response, next) {
   var filteredId = path.parse(request.params.pageId).base;
   fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
     if (err) {
@@ -52,9 +132,9 @@ app.get('/page/:pageId', function (request, response, next) {
         sanitizedTitle,
         list,
         `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
-        `<a href='/create'>create</a>
-        <a href='/update/${sanitizedTitle}'>update</a>
-        <form action= "/delete_process" method = "post" onsubmit= "return confirm('ARE YOU SURE?')">
+        `<a href='/topic/create'>create</a>
+        <a href='/topic/update/${sanitizedTitle}'>update</a>
+        <form action= "/topic/delete_process" method = "post" onsubmit= "return confirm('ARE YOU SURE?')">
           <input type ="hidden" name="id" value="${sanitizedTitle}">
           <input type ="submit" value= "delete">
         </form>
@@ -64,81 +144,6 @@ app.get('/page/:pageId', function (request, response, next) {
     }
   });
 });
-
-app.get('/create', (request, response) => {
-  var title = 'WEB_CREATE';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-      <form action = "http://localhost:3000/create_process" method= "post">
-        <p> <input type ="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name ="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type ="submit">
-        </p>
-      </form>
-    `, '');
-  response.send(html);
-});
-
-app.post('/create_process', (request, response) => {
-  console.log(request.list);
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  console.log("title: ", title, "\n", "des: ", description);
-  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    response.writeHead(302, { Location: `/page/${title}` });
-    response.end();
-  })
-})
-
-app.get('/update/:pageId', (request, response) => {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-            <form action="/update_process" method="post">
-                <input type="hidden" name="id" value="${title}">
-                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-                <p>
-                    <textarea name="description"
-                        placeholder="description">${description}</textarea>
-                </p>
-                <p>
-                    <input type="submit">
-                </p>
-            </form>
-            `,
-      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-    );
-    response.send(html);
-  });
-});
-
-app.post('/update_process', (request, response) => {
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, description, function (err1) {
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err2) {
-      response.redirect(`/page/${title}`);
-      response.end();
-    })
-  })
-})
-app.post('/delete_process', (request, response) => {
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function () {
-    response.redirect('/');
-  })
-})
 
 app.use(function (req, res, next) {
   res.status(404).send("Sorry 404 Error.")
