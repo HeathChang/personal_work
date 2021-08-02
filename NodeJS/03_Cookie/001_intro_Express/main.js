8351c8f5710cb3bd30297d5a11bbed2e8d7f1eef
HeathChang <jannhyunsoo@gmail.com>
2021-08-02 21:36:10 +0900

    210802_nodeJs_Cookie_3

--- NodeJS/03_Cookie/001_intro_Express/main.js
+++ NodeJS/03_Cookie/001_intro_Express/main.js
@@ -2,9 +2,17 @@ var http = require('http');
 var cookie = require('cookie');
 
 http.createServer(function (request, response) {
-  //response.writeHead(200, {
-  //  'set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
-  //});
+  response.writeHead(200, {
+    'set-Cookie': [
+      'yummy_cookie=choco',
+      'tasty_cookie=strawberry',
+      //한달동안 유효
+      `Permanent = cookies; Max-Age = ${60 * 60 * 24 * 30}`,
+      'Secure = Secure; Secure',
+      //웹 브라우저와 웹 서버가 HTTP 프로토콜로 통신하는 경우, 쿠키를 전송하지만, JS는 쿠키값을 가져올 수 없음
+      'HttpOnly = HttpOnly; HttpOnly'
+    ]
+  });
   var cookies = {};
   if (request.headers.cookie != undefined) {
     cookies = cookie.parse(request.headers.cookie);
