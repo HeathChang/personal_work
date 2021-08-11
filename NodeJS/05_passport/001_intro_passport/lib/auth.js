a531f83ac498487beb38c83574973ac883f4e119
HeathChang <jannhyunsoo@gmail.com>
2021-08-11 17:24:40 +0900

    210811_nodeJs_passport_3

--- NodeJS/05_passport/001_intro_passport/lib/auth.js
+++ NodeJS/05_passport/001_intro_passport/lib/auth.js
@@ -1,15 +1,17 @@
 module.exports = {
-    isOwner:function(request, response) {
-        if (request.session.is_logined) {
+    isOwner: function (request, response) {
+        console.log("request.user in auth.js: ", request.user);
+        if (request.user) {
+            console.log("auth.js에서 request.user 체크");
             return true;
         } else {
             return false;
         }
     },
-    statusUI:function(request, response) {
+    statusUI: function (request, response) {
         var authStatusUI = '<a href="/auth/login">login</a>'
         if (this.isOwner(request, response)) {
-            authStatusUI = `${request.session.nickname} | <a href="/auth/logout">logout</a>`;
+            authStatusUI = `${request.user.nickname} | <a href="/auth/logout">logout</a>`;
         }
         return authStatusUI;
     }
