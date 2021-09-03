80542f8ed95231a7a88bf0345f1efa4127a2d451
HeathChang <jannhyunsoo@gmail.com>
2021-09-03 15:41:13 +0900

    210903_python_2

--- Python/Python101/Basic_Algo/Practice001_Prime.py
+++ Python/Python101/Basic_Algo/Practice001_Prime.py
@@ -1,3 +1,4 @@
+import math
 #소수 판단: 비효율적 → 하나씩 차례대로 나누기 떄문
 def is_primeNumber (x):
   for i in range (2,x):
@@ -9,11 +10,10 @@ print(is_primeNumber(4))
 print(is_primeNumber(7))    
 
 #소수 판단: 제곱근을 사용
-import math
 def is_primeNumber(x):
   for i in range(2,int(math.sqrt(x))+1):
     #만약 X가 해당 수로 나눠떨어지면 소수아님
     if(x % i == 0):return False
     return True
 print(is_primeNumber(4))    
-print(is_primeNumber(7))   
+print(is_primeNumber(7))  
