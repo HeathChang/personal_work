d0456d676675a53f55876ff9ac870b5894f71804
HeathChang <jannhyunsoo@gmail.com>
2021-09-08 16:22:45 +0900

    210908_mongo_6

--- Mongo&Node/task-manager/mongoJS_Folder/04_mongo_Update.js
+++ Mongo&Node/task-manager/mongoJS_Folder/04_mongo_Update.js
@@ -19,13 +19,15 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     _id: new ObjectID("613843414cc5f916e402f299")
   }, {
     $set: {
-      name: "HSC",
-      Age: 29
+      name: "Holly",
+      Age: 30
     }
     // $inc: {
     //   Age: 2
     // }
   }).then((result) => {
+    console.log("matchedCount:", result.matchedCount); //일치하는 데이터 개수
+    console.log("modifiedCount:", result.modifiedCount); //실제 변경된게 있는지 여부
     console.log(result);
   }).catch((error) => {
     console.log(error);
