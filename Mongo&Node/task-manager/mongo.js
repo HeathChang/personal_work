9d723d68ab3033e40aed2635ba8caf8d4bbfa08a
HeathChang <jannhyunsoo@gmail.com>
2021-09-08 17:51:31 +0900

    210908_mongo_7

--- Mongo&Node/task-manager/mongo.js
+++ Mongo&Node/task-manager/mongo.js
@@ -1,6 +1,3 @@
-const mongodb = require('mongodb')
-// const MongoClient = mongodb.MongoClient
-// const ObjectID = mongodb.ObjectID
 const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
@@ -13,26 +10,10 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
   }
   const db = client.db(databaseName);
 
-  //Delete
-  //결과가 있든 없든 (에러가 발생한게 아니면) 에러는 아님.
-  db.collection('users').deleteMany({
-    Age: 28
-  }).then((result) => {
-    console.log("삭제 수: ", result.deletedCount)
-    if (result.deletedCount == 0) {
-      console.log("일치하는 데이터가 없습니다");
-    } else {
-      console.log("Correctly removed: ", result.deletedCount);
-    }
-  }).catch((error) => {
-    console.log("Error");
-  })
-
-
 })
 
 
 
-
+//ODM O
 
 
