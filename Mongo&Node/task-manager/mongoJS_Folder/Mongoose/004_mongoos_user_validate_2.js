37fd88d5b42780180013719505e728f0717fee75
HeathChang <jannhyunsoo@gmail.com>
2021-09-11 17:18:20 +0900

    210911_mongo_4

--- Mongo&Node/task-manager/mongoJS_Folder/Mongoose/004_mongoos_user_validate_2.js
+++ Mongo&Node/task-manager/mongoJS_Folder/Mongoose/004_mongoos_user_validate_2.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true
 })
@@ -8,19 +10,23 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
   name: {
     type: String,
-    required: true //Path `name` is required.
+    required: true, //Path `name` is required.
+    trim: true
   },
   email: {
     type: String,
     required: true,
+    trim: true, //양옆 띄어쓰기 없애기
+    lowercase: true, //소문자로 저장
     validate(value) {
-      if (!validator.isEmail(Email)) {
-        throw new Error('Email is not Perfect')
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
       }
     }
   },
   age: {
     type: Number,
+    default: 0,
     validate(value) {
       if (value < 0) {
         throw new Error('Age must be a positive Number')
@@ -31,9 +37,9 @@ const User = mongoose.model('User', {
 
 ////Data Inputs
 const me = new User({
-  name: "Heath",
-  email: 'mike@",
-  age: 29
+  name: "     Heath      ",
+  email: 'MIKE@HEAD.IO ',
+  //age: 29
   //_message: 'User validation failed';
 })
 
