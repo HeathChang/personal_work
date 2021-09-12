60714e0cd99724d08e4ba425828b7c2782e9c0ee
HeathChang <jannhyunsoo@gmail.com>
2021-09-12 15:21:30 +0900

    210912_mongo_1

--- Mongo&Node/task-manager/mongoJS_Folder/Mongoose/E086_mongoos_user&task.js
+++ Mongo&Node/task-manager/mongoJS_Folder/Mongoose/E086_mongoos_user&task.js
@@ -38,7 +38,7 @@ const User = mongoose.model('User', {
     default: 0,
     validate(value) {
       if (value < 0) {
-        throw new Error('Age must be a postive number')
+        throw new Error('Age must be a positive number')
       }
     }
   }
