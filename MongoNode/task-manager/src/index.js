2f2bebfb2c927cca2f286b53c356ebabb352c357
heathchang <jannhyunsoo@gmail.com>
2021-09-15 12:27:51 +0900

    210915_delete

--- MongoNode/task-manager/src/index.js
+++ MongoNode/task-manager/src/index.js
@@ -64,6 +64,19 @@ app.patch('/users/:id',async(req,res)=>{
     }
 })
 
+app.delete('/users/:id',async(req,res)=>{
+    try{
+    const user= await User.findByIdAndDelete(req.params.id)
+    if(!user){
+        res.status(404).send({error:"cannot find user"});
+    }
+    res.send(user);
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
