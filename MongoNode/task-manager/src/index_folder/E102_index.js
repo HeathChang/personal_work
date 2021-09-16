c6cadcf5ce26014751c4a6a1b6e745ac0c864518
heathchang <jannhyunsoo@gmail.com>
2021-09-16 22:24:47 +0900

    210916_mongo_3

--- MongoNode/task-manager/src/index_folder/E102_index.js
+++ MongoNode/task-manager/src/index_folder/E102_index.js
@@ -19,21 +19,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-
-//hashing alg: one-way pass
-const bcrypt = require('bcryptjs')
-
-const myFunction = async() =>{
-    const password = "Red12345!"
-    const hashPassword = await bcrypt.hash(password,8);
-    console.log(password);
-    console.log(hashPassword);
-
-    const isMatch = await bcrypt.compare(password,hashPassword);
-    console.log(isMatch);
-}
-myFunction();
-
-//
-
