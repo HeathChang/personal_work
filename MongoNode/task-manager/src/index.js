6c70c4b824d15745c085d6f35c7f492e4ba855fa
heathchang <jannhyunsoo@gmail.com>
2021-09-18 16:53:24 +0900

    210918_mongo_2

--- MongoNode/task-manager/src/index.js
+++ MongoNode/task-manager/src/index.js
@@ -7,6 +7,7 @@ const taskRouter = require ('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
+const jwt = require('jsonwebtoken')
 
 //parse incoming JSON to Object
 app.use(express.json())
@@ -20,4 +21,17 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
+
+//JWT
+const myFunction = async() =>{
+    //object: data that will be embedded
+    //creating token
+    const token = jwt.sign({_id: "abc123"},'thisismynewcourse',{expiresIn: '7 days'})
+    console.log("token>>",token)
+
+    //verify
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log("data>>",data);
+}
+myFunction();
 
