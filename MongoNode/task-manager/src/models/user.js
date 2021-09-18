ff50863289801c69417d0c32a2adf56ea41d7acc
heathchang <jannhyunsoo@gmail.com>
2021-09-18 17:16:51 +0900

    210918_mongo_4

--- MongoNode/task-manager/src/models/user.js
+++ MongoNode/task-manager/src/models/user.js
@@ -39,13 +39,21 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number')
             }
         }
-    }
+    },
+    tokens:[{
+        token: {
+            type: String,
+            required:true
+        }
+    }]
 })
 
 //instance method
 userSchema.methods.generateAuthToken = async function(){
     const user = this
     const token = jwt.sign({_id: user._id.toString()},'thisismynewcourse')
+    user.tokens = user.tokens.concat({token : token})
+    await user.save()
     return token
 }
 
