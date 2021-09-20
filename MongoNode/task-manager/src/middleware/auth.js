68753d09940620c35b742e6685fb54b7eb14edf2
heathchang <jannhyunsoo@gmail.com>
2021-09-21 00:09:10 +0900

    210921_mongo_1

--- MongoNode/task-manager/src/middleware/auth.js
+++ MongoNode/task-manager/src/middleware/auth.js
@@ -14,10 +14,11 @@ const auth = async (req,res,next) =>{
         const user = await User.findOne({_id: decoded._id,'tokens.token':token})
         console.log("checking user>>",user);
         //정보
-        
+
         if(!user){
             throw new Error()
         }
+        req.token = token 
         req.user = user
         next()
     }catch(e){
--- MongoNode/task-manager/src/routers/user.js
+++ MongoNode/task-manager/src/routers/user.js
@@ -65,6 +65,18 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+router.post('/users/logout', auth, async(req,res)=>{
+    try{
+        req.user.tokens = req.user.tokens.filter((token)=>{
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
 router.patch('/users/:id',async(req,res)=>{
     //update할 내용
     const updates = Object.keys(req.body)
