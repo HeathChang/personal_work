a894ba25121a86f0c5c8bdc88a8dd269c94b1251
heathchang <jannhyunsoo@gmail.com>
2021-09-28 00:07:49 +0900

    210928_mongo_1

--- MongoNode/task-manager/src/index.js
+++ MongoNode/task-manager/src/index.js
@@ -27,6 +27,12 @@ const port = process.env.PORT || 3000
 // })
 
 const multer = require('multer')
+const upload = multer({
+    dest:'images'
+})
+app.post('/upload',upload.single('upload'),(req,res)=>{
+    res.send()
+})
 
 //parse incoming JSON to Object
 app.use(express.json())
