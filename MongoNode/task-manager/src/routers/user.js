d49d1ec867652ddf46f62ae17afe691e2610e283
heathchang <jannhyunsoo@gmail.com>
2021-09-28 00:20:24 +0900

    210928_mongo_2

--- MongoNode/task-manager/src/routers/user.js
+++ MongoNode/task-manager/src/routers/user.js
@@ -2,6 +2,7 @@ const express = require ('express')
 const { update } = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
+const multer = require('multer')
 
 const User = require('../models/user')
 
@@ -124,6 +125,13 @@ router.delete('/users/me',auth,async(req,res)=>{
     }catch(e){
         res.status(500).send(e)
     }
+})
+
+const upload = multer({
+    dest:'avatars'
+})
+router.post('/users/me/avatar',upload.single('avatar'),(req,res)=>{
+    res.send()
 })
 
 module.exports = router
