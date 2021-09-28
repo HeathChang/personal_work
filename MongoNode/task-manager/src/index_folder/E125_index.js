d6d59e26a8f8e8236c31b94422be3f87bd7bf988
heathchang <jannhyunsoo@gmail.com>
2021-09-28 23:38:55 +0900

    210929_mongo_3

--- MongoNode/task-manager/src/index_folder/E125_index.js
+++ MongoNode/task-manager/src/index_folder/E125_index.js
@@ -39,12 +39,11 @@ const upload = multer({
         callback(undefined, true)
     }
 })
-
-//keyvalue
+const errorMiddleware = (req,res,next) =>{
+    throw new Error('From my middleware')
+}
 app.post('/upload',upload.single('upload'),(req,res)=>{
     res.send()
-}, (error,req,res,next)=>{
-    res.status(400).send({error: error.message})
 })
 
 
