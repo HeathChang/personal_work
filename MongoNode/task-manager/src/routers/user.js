c3e6f219daac34733b00e9d52b0e7ea61b8fcdfe
heathchang <jannhyunsoo@gmail.com>
2021-09-29 23:17:45 +0900

    210929_mongo_4

--- MongoNode/task-manager/src/routers/user.js
+++ MongoNode/task-manager/src/routers/user.js
@@ -3,6 +3,7 @@ const { update } = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 const multer = require('multer')
+const sharp = require('sharp')
 
 const User = require('../models/user')
 
@@ -143,7 +144,9 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar',auth,upload.single('avatar'),async (req,res)=>{
-    req.user.avatar = req.file.buffer
+    //req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({width:250 ,height:250}).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 },(error,req,res,next)=>{
