d8a2b099bcbd53dc558e44da666893c40600e564
heathchang <jannhyunsoo@gmail.com>
2021-10-04 17:41:39 +0900

    211004_react_1

--- React/ModernReact/my-app/src/index.js
+++ React/ModernReact/my-app/src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//1. import the react and reactDom libraries
+import React from 'react'
+import ReactDOM  from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//2. create a react component
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//3. Take the react component and show it on the screen
