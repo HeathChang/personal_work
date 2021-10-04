59cde8638503b62d9215b3a1002d01affc5783cd
heathchang <jannhyunsoo@gmail.com>
2021-10-04 17:46:57 +0900

    211004_react_2

--- React/ModernReact/my-app/src/index.js
+++ React/ModernReact/my-app/src/index.js
@@ -3,6 +3,13 @@ import React from 'react'
 import ReactDOM  from 'react-dom'
 
 //2. create a react component
-
+const App = function() {
+    return <div> Hi There  </div>
+}
 
 //3. Take the react component and show it on the screen
+ReactDOM.render(
+    <App/>,
+    //reference to the root
+    document.querySelector('#root')
+)
