0c9f178c61ba3a01a034f4c236e2dc4be3bb0892
heathchang <jannhyunsoo@gmail.com>
2021-10-05 23:39:15 +0900

    211005_react@home_4

--- React/ModernReact/components/src/index.js
+++ React/ModernReact/components/src/index.js
@@ -8,62 +8,7 @@ import faker from 'faker'
 const App = () => {
     return (
         <div className ="ui container comments"> 
-            <div className = "comment">
-                <a href="/" className="avatar">
-                    <img alt = "avatar" src = {faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href ="/" className = "author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            Today at 6:00 PM
-                        </span>
-                    </div>
-                        <div className ="text">
-                            Nice blog post ! XD
-                        </div>
-                </div>
-            </div>
-
-            <div className = "comment">
-                <a href="/" className="avatar">
-                    <img alt = "avatar" src = {faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href ="/" className = "author">
-                        Taylor
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            Today at 6:01 PM
-                        </span>
-                    </div>
-                        <div className ="text">
-                            lol
-                        </div>
-                </div>
-            </div>
-
-            <div className = "comment">
-                <a href="/" className="avatar">
-                    <img alt = "avatar" src = {faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href ="/" className = "author">
-                        Andrew
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            Today at 6:10 PM
-                        </span>
-                    </div>
-                        <div className ="text">
-                            Thx
-                        </div>
-                </div>
-            </div>
+           
         </div>
     );
 }
