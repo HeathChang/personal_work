6560602372e0da1a047ebcabd7623e2a68a6de54
heathchang <jannhyunsoo@gmail.com>
2021-10-05 23:24:13 +0900

    211005_react@home_3

--- React/ModernReact/components/src/index.js
+++ React/ModernReact/components/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import  ReactDOM  from 'react-dom'
+
+//faker: to use fake data
+import faker from 'faker'
+
+
 const App = () => {
     return (
         <div className ="ui container comments"> 
             <div className = "comment">
                 <a href="/" className="avatar">
-                    <img alt = "avatar"/>
+                    <img alt = "avatar" src = {faker.image.avatar()}/>
                 </a>
                 <div className="content">
-                    <a href ="/" className="author">
+                    <a href ="/" className = "author">
                         Sam
                     </a>
                     <div className="metadata">
@@ -18,6 +23,44 @@ const App = () => {
                     </div>
                         <div className ="text">
                             Nice blog post ! XD
+                        </div>
+                </div>
+            </div>
+
+            <div className = "comment">
+                <a href="/" className="avatar">
+                    <img alt = "avatar" src = {faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href ="/" className = "author">
+                        Taylor
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            Today at 6:01 PM
+                        </span>
+                    </div>
+                        <div className ="text">
+                            lol
+                        </div>
+                </div>
+            </div>
+
+            <div className = "comment">
+                <a href="/" className="avatar">
+                    <img alt = "avatar" src = {faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href ="/" className = "author">
+                        Andrew
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            Today at 6:10 PM
+                        </span>
+                    </div>
+                        <div className ="text">
+                            Thx
                         </div>
                 </div>
             </div>
