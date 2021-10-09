0629b5e13b2fca97bea1d36e23cd3c219e377899
heathchang <jannhyunsoo@gmail.com>
2021-10-09 17:08:20 +0900

    211009_react@home_4

--- React/ModernReact/seasons/src/index.js
+++ React/ModernReact/seasons/src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
+class App extends React.Component {
+  //기본값
+  constructor(props) {
+    // super reference to the parents
+    super(props);
+    //this is the only time to do direct assignment
+    this.state = { lat: null }; //because we dnt know what latitude value is
 
-class App extends React.Component{
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position)=> console.log(position),
-            (err) => console.log(err)
-        )
-        return <div> Latitude:  </div>
-    }
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //setState is  to update state.
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  //React says we have to define render.
+  render() {
+    return <div> Latitude: {this.state.lat}</div>;
+  }
 }
-ReactDOM.render(<App/>, document.querySelector('#root'))
-
+ReactDOM.render(<App />, document.querySelector("#root"));
