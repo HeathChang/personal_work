660ee23ddd89814751d605af3b74c8832e489df1
heathchang <jannhyunsoo@gmail.com>
2021-10-12 23:55:37 +0900

    211013_react@home_9

--- React/ModernReact/seasons/src/Spinner.js
+++ React/ModernReact/seasons/src/Spinner.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const Spinner = () => {
+const Spinner = (props) => {
     return (
     <div className= "ui active dimmer">
-        <div className="ui big text loader">Now Loading...</div>
+        <div className="ui big text loader">{props.message}</div>
     </div>
     )
 }
+
+Spinner.defaultProps = {
+    message: "Now Loading..."
+}
+
+
 export default Spinner;
