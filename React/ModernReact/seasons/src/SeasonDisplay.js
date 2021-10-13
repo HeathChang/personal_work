3eeefcf6982068b4fddd9bac073e4d83befe7f9f
heathchang <jannhyunsoo@gmail.com>
2021-10-13 20:52:35 +0900

    211013_react@home_2

--- React/ModernReact/seasons/src/SeasonDisplay.js
+++ React/ModernReact/seasons/src/SeasonDisplay.js
@@ -1,6 +1,20 @@
 import "./SeasonDisplay.css";
 import React from "react";
 
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
+    console.log(seasonConfig.winter);
+  
+    return (
+      <div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${iconName} icon`} />
+        <h1>{text}</h1>
+        <i className={`icon-right massive ${iconName} icon`} />
+      </div>
+    );
+  };
+
 const seasonConfig = {
   summer: {
     text: "Lets hit the beach !",
@@ -20,18 +34,6 @@ const getSeason = (lat, month) => {
   }
 };
 
-const SeasonDisplay = (props) => {
-  const season = getSeason(props.lat, new Date().getMonth());
-  const { text, iconName } = seasonConfig[season];
-  console.log(seasonConfig.winter);
 
-  return (
-    <div className={`season-display ${season}`}>
-      <i className={`icon-left massive ${iconName} icon`} />
-      <h1>{text}</h1>
-      <i className={`icon-right massive ${iconName} icon`} />
-    </div>
-  );
-};
 
 export default SeasonDisplay;
