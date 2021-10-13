4a580fefdbd369922c75c27d1f857919fc41e697
heathchang <jannhyunsoo@gmail.com>
2021-10-13 21:30:38 +0900

    211013_react@home_6

--- React/ModernReact/pics/src/components/app.js
+++ React/ModernReact/pics/src/components/app.js
@@ -5,10 +5,10 @@ import SearchBar from "./SearchBar";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container" style ={{marginTop: '10px'}}>
       <SearchBar />
     </div>
-  );
+  )
 };
 
 export default App;
