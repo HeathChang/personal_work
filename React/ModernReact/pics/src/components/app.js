e67ad4f31d2fb857e3eb51631d27b8b9f7d0f8af
heathchang <jannhyunsoo@gmail.com>
2021-10-13 21:22:51 +0900

    211013_react@home_5

--- React/ModernReact/pics/src/components/app.js
+++ React/ModernReact/pics/src/components/app.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import SearchBar from "./SearchBar";
 
 const App = () => {
-    return <div>App</div>
-}
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 export default App;
