ccdde0670d0f1a3f259c3c7cb6315254e1c7598c
heathchang <jannhyunsoo@gmail.com>
2021-10-14 22:01:34 +0900

    211014_react@home_1

--- React/ModernReact/pics/src/components/SearchBar.js
+++ React/ModernReact/pics/src/components/SearchBar.js
@@ -2,13 +2,19 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  onInuputChange(event) {
+      let text= event.target.value
+      console.log(text);
+  }
+
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label> Image Search </label>
-            <input type= "text"/>
+            <input type="text" onChange={this.onInuputChange} />
           </div>
         </form>
       </div>
