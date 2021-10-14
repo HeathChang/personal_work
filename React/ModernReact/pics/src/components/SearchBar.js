5597d0286cccaeaa1d35db163c60ea9f451b556b
heathchang <jannhyunsoo@gmail.com>
2021-10-14 22:30:37 +0900

    211014_react@home_2

--- React/ModernReact/pics/src/components/SearchBar.js
+++ React/ModernReact/pics/src/components/SearchBar.js
@@ -2,10 +2,12 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInuputChange(event) {
-      let text= event.target.value
-      console.log(text);
-  }
+//   onInuputChange(event) {
+//       let text= event.target.value
+//       console.log(text);
+//   }
+
+    state = {term : ''};
 
 
   render() {
@@ -14,7 +16,11 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
             <label> Image Search </label>
-            <input type="text" onChange={this.onInuputChange} />
+            <input type="text" value={this.state.term} onChange={(e)=> {this.setState({term: e.target.value.toUpperCase()})}}/>
+            {/* 
+            <input type="text" onChange={this.onInuputChange} /> 
+            <input type="text" onChange={(event)=>{console.log(event.target.value)}} />  
+             */}
           </div>
         </form>
       </div>
