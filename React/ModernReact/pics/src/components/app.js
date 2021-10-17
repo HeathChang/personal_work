53e07cececbab1ecd2f66e43ca774c38b5ad3a36
heathchang <jannhyunsoo@gmail.com>
2021-10-17 22:33:11 +0900

    211017_react@home_3

--- React/ModernReact/pics/src/components/app.js
+++ React/ModernReact/pics/src/components/app.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response= await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID NiTif1HhcgTVaT4CvHQW114LHUO_GqaJCmQRuCGU9yI', 
       }
-    });
+    })
+   console.log(response.data.results);
   }
 
   render() {
