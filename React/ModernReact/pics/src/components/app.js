71aba282ab2a2c1ae27ac571a37e1e8fe22c5a61
heathchang <jannhyunsoo@gmail.com>
2021-10-17 23:00:31 +0900

    211017_react@home_6

--- React/ModernReact/pics/src/components/app.js
+++ React/ModernReact/pics/src/components/app.js
@@ -3,6 +3,7 @@ import React from "react";
 //components
 import SearchBar from "./SearchBar";
 import unsplash from "../api/unsplash";
+import ImageList from "./imageList";
 
 class App extends React.Component {
   state = { images: [] };
@@ -19,7 +20,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images= {this.state.images}/>
       </div>
     );
   }
---
+++ React/ModernReact/pics/src/components/imageList.js
@@ -0,0 +1,7 @@
+import React from "react";
+const ImageList = (props) => {
+    console.log("ImageList from imageList.js>> ", props.images);
+    return <div>ImageList </div>
+}
+
+export default ImageList;
