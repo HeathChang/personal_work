5ba971791d4ab74d942cb1485eaa0b200c6a7b2b
heathchang <jannhyunsoo@gmail.com>
2021-10-18 23:24:37 +0900

    211018_react@home_4

--- React/ModernReact/pics/src/components/imageList.js
+++ React/ModernReact/pics/src/components/imageList.js
@@ -1,12 +1,12 @@
-import './ImageList.css';
+import "./ImageList.css";
 import React from "react";
-
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-    const images= props.images.map(({description,id,urls})=>{
-        return <img alt={description} key={id} src = {urls.regular}/>
-    }) //object type
-    return <div className="image-list">{images}</div>
-}
+  const images = props.images.map(({ description, id, urls }) => {
+    return <ImageCard key={image.id} image={image}/>;
+  }); //object type
+  return <div className="image-list">{images}</div>;
+};
 
 export default ImageList;
