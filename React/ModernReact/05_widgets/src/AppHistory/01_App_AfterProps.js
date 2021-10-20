9cacb8f1b991b6b36fef5a7fca4fceecefd2e216
heathchang <jannhyunsoo@gmail.com>
2021-10-20 23:46:18 +0900

    211020_vue@home_3

---
+++ React/ModernReact/05_widgets/src/AppHistory/01_App_AfterProps.js
@@ -0,0 +1,29 @@
+//from npm
+import React from "react";
+
+//components
+import Accordion from "./components/Accordion";
+//import Accordion from "./components/Accordion";
+
+const items = [
+    {
+        title: 'What is React',
+        content: 'React is a front end JS framework'
+    },
+    {
+        title: 'Why use React',
+        content: 'React is a famous JS library among engs'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by making components'
+    }
+]
+
+export default () => {
+  return (
+    <div>
+      <Accordion itemsFromApp={items}/>
+    </div>
+  );
+};
