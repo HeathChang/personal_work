d072c0c0140c9302f22d59339bae8a608f1a02f9
heathchang <jannhyunsoo@gmail.com>
2021-10-26 20:56:23 +0900

    211026_react@home_6

--- React/ModernReact/05_widgets/src/App.js
+++ React/ModernReact/05_widgets/src/App.js
@@ -4,6 +4,8 @@ import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 import Route from "./components/Route";
+import Header from "./components/Header";
+
 const items = [
   {
     title: "What is React?",
@@ -55,6 +57,7 @@ const App = () => {
   const [selected, setSelected] = useState(options[0])
   return (
     <div>
+      <Header/>
       <Route path="/"><Accordion items={items} /* This becomes children*/ /></Route>
       <Route path="/dropdown"><Dropdown label="Select a color" options={options} selected={selected} onSelectedChange={setSelected} /* This becomes children*/ /></Route>
       <Route path="/search"><Search  /* This becomes children*/ /></Route>
---
+++ React/ModernReact/05_widgets/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <div className="ui secondary point menu">
+            <a href="/" className="item">Accordion</a>
+            <a href="/search" className="item">Search</a>    
+            <a href="/dropdown" className="item">Dropdown</a>    
+            <a href="/translate" className="item">Translate</a>    
+    
+        </div>
+    )
+}
+export default Header;
