fec9211ce3dcb9b32bf757bbcd052bad350a1f78
heathchang <jannhyunsoo@gmail.com>
2021-10-28 19:52:26 +0900

    211028_react@home_3

--- React/ModernReact/05_widgets/src/components/Link.js
+++ React/ModernReact/05_widgets/src/components/Link.js
@@ -2,7 +2,9 @@ import React from "react";
 const Link = ({className, href, children}) => {
     const onClick = (event) => {
         event.preventDefault();
-        window.history.pushState({},'',href)
+        window.history.pushState({},'',href);
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
     }
 
     return <a onClick={onClick} className={className} href={href}>{children}</a>
