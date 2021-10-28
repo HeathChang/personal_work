ea8c63d03d3118c866d2b955ded1cb188320838b
heathchang <jannhyunsoo@gmail.com>
2021-10-28 20:50:56 +0900

    211028_react@home_4

--- React/ModernReact/05_widgets/src/components/Route.js
+++ React/ModernReact/05_widgets/src/components/Route.js
@@ -1,20 +1,23 @@
 import { useEffect, useState } from "react";
 
 
-const Route = ({path, children}) => {
-    useEffect(()=>{
+const Route = ({ path, children }) => {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+    useEffect(() => {
         const onLocationChange = () => {
             console.log('Location Change');
+            setCurrentPath(window.location.pathname)
         }
         window.addEventListener('popstate', onLocationChange)
         return () => {
-            window.removeEventListener('popstate',onLocationChange)
+            window.removeEventListener('popstate', onLocationChange)
         }
-    },[])
+    }, [])
 
     return window.location.pathname === path
-    ? children
-    : null
+        ? children
+        : null
 }
 
 export default Route;
