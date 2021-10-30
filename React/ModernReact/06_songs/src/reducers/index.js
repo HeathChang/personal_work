e43e5dcf78dc4d9b02b4dbe3768b0187d1833780
heathchang <jannhyunsoo@gmail.com>
2021-10-30 18:55:18 +0900

    211030_react@home_5

---
+++ React/ModernReact/06_songs/src/reducers/index.js
@@ -0,0 +1,17 @@
+const songsReducer = () => {
+    return [
+        { title: 'No Scrubs' , duration: '4:05'},
+        { title: 'Barbie girl' , duration: '5:15'},
+        { title: 'I want it That Way' , duration: '2:30'},
+        { title: 'All Starts' , duration: '3:10'},
+    ]
+}
+
+const selectedSongReducer = (selectedSong =null, action) => {
+    if(action.type === 'SONG_SELECTED'){
+        console.log("payload check: ", action.payload);
+        return action.payload;
+    }
+
+    return selectedSong;
+}
