12fce5c3788ea325c57e064d71805d53cd8a079f
heathchang <jannhyunsoo@gmail.com>
2021-10-30 18:48:23 +0900

    211030_react@home_4

--- React/ModernReact/06_songs/src/index.js
+++ React/ModernReact/06_songs/src/index.js
@@ -1,6 +1,7 @@
+//npm download
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+//import file
 import App from './components/App';
 
 ReactDOM.render(<App/>,document.querySelector('#root'))
