204cedc0782601a7351714b346fc1b9c63ddf160
heathchang <jannhyunsoo@gmail.com>
2021-10-31 23:15:46 +0900

    211031_react@home_3

--- React/ModernReact/06_songs/src/components/SongList.js
+++ React/ModernReact/06_songs/src/components/SongList.js
@@ -1,10 +1,16 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-class SongList extends Component{
-    render(){
+class SongList extends Component {
+    render() {
+        //this.props == {songs: state.songs}
+        console.log(this.props);
         return <div>SongList</div>
     }
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+}
+
+export default connect(mapStateToProps)(SongList);
