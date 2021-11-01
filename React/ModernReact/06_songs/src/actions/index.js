ae5ec0e1525561e6a2bb41c80ef669ec7f09ed98
heathchang <jannhyunsoo@gmail.com>
2021-11-01 20:44:53 +0900

    211101_react@home_3

--- React/ModernReact/06_songs/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-// Action Creater: state change 용도 
-export const selectSong = (song) => {
-    //return an action
-    console.log("checking song in action", song);
-    return {
-        type: 'SONG_SELECTED',
-        payload: song
-    }
-}
