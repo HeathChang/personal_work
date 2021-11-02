e892cff107b000ded380b7dd024ab8ab87096f5e
heathchang <jannhyunsoo@gmail.com>
2021-11-02 21:08:28 +0900

    211102_react@home_7

--- React/ModernReact/06_songs/src/index.js
+++ React/ModernReact/06_songs/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
 //////////////////////////////////
 import App from './components/App';
 import reducers from './reducers'
@@ -12,6 +11,6 @@ ReactDOM.render(
     <Provider store={createStore(reducers)}>
         <App />
     </Provider>,
-     document.querySelector('#root')
-     );
+    document.querySelector('#root')
+);
 
