05c2807290807c3ecb6b5430ee747950d28f0b2d
heathchang <jannhyunsoo@gmail.com>
2021-11-02 20:34:03 +0900

    211102_react@home_4

---
+++ React/ModernReact/06_songs/src/reducers/index.js
@@ -0,0 +1,19 @@
+const songsReducer = () => {
+    return [
+        { title: 'No Scrubs', duration: '4:01' },
+        { title: 'Macarena', duration: '4:02' },
+        { title: 'All Start', duration: '4:03' },
+        { title: 'I want it That way', duration: '4:04' }
+
+    ]
+};
+
+
+const selectedSongReducer = (selectedSong = null,action) => {
+    if(action.type === 'SONG_SELECTED'){
+        console.log("check action on reducers: ",action);
+        return action.payload
+    }
+    return selectedSong;
+
+};
