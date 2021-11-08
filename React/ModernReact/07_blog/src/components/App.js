0e0c1f3d6a7ccd06b5ebe54dcd164c83a0fbde31
heathchang <jannhyunsoo@gmail.com>
2021-11-08 23:32:50 +0900

    211108_2

--- React/ModernReact/07_blog/src/components/App.js
+++ React/ModernReact/07_blog/src/components/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return (
-    <div className="ui container">
-      App
-    </div>
-  );
-}
+  return <div className="ui container">App</div>;
+};
 
 export default App;
