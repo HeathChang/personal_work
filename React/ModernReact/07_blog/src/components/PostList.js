25eeaa003e2a565a40d5133d9d2dcd8e326e2bc7
heathchang <jannhyunsoo@gmail.com>
2021-11-10 21:11:56 +0900

    211110_5

--- React/ModernReact/07_blog/src/components/PostList.js
+++ React/ModernReact/07_blog/src/components/PostList.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from 'react-redux';
 
 import { fetchPosts } from '../actions'
-
+import UserHeader from "./UserHeader";
 class PostList extends React.Component {
     componentDidMount() {
         this.props.fetchPosts();
@@ -11,14 +11,16 @@ class PostList extends React.Component {
         return this.props.posts.map((post) => {
             return (
                 <div className="item" key={post}>
-                    <i className="large middle aligned icon user"/>
-                        <div className="content">
-                            <div className="description">
-                                <h2>{post.title}</h2>
-                                <p>{post.body}</p>
-                            </div>
+                    <i className="large middle aligned icon user" />
+
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
                         </div>
-                    
+                        <UserHeader userId={post.userId} />
+                    </div>
+
                 </div>
             )
         })
