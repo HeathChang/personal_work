5d1b334a1cafcc99339cf03bb7b733ad6b8487de
heathchang <jannhyunsoo@gmail.com>
2021-11-21 23:43:31 +0900

    211121_3

--- React/ModernReact/07_blog/src/components/UserHeader.js
+++ React/ModernReact/07_blog/src/components/UserHeader.js
@@ -9,14 +9,29 @@ class UserHeader extends React.Component {
     }
 
     render() {
-        return <div> User Header</div>
+        const user = this.props.users.find((user) => (
+             user.id === this.props.userId
+        ))
+        //중괄호는 return 꼭 써줘야함. 
+        if (!user) {
+            return null
+        }
+        return <div className="header"> {user.name} </div>
     }
 }
 
-export default connect(null, { fetchUser })(UserHeader);
+const mapStateToProps = (state) => {
+    return {
+        users: state.users
+    }
+}
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
 
 
 //1. 컴포넌트를 생성 후, 메인컴포넌트에 연결 
 //2. 이후 Connect 와 fetchUser를  import 하여 actions 가져옴
 //3. connect를 export에서 사용하고, 현재 mapStateToProps가 없기 때문에 null 부여 
 //4. 이후 2번쨰 args로는 임포트한 fetchUser를 연결해준다. 
+//5. 이후 reducers를 생성하고 reducers내 index.js에 import 후, combinedReducers에 넣어줌 
+//6. 이후, 현재 파일로 연결하여 mapStateToProps 생성 후, connect와 연결
