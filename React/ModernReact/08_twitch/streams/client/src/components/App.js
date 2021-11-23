4a1327fbb0daced34dc04d695b12bb8c174b4e67
heathchang <jannhyunsoo@gmail.com>
2021-11-23 23:24:23 +0900

    211123_react@home_2_basic-setup

--- React/ModernReact/08_twitch/streams/client/src/components/App.js
+++ React/ModernReact/08_twitch/streams/client/src/components/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route , Link } from 'react-router-dom';
 
 const PageOne = () => {
-    return <div> page one</div>
+    return <div> page one
+        <Link to="/page/2">Navigate to page 2</Link>
+    </div>
 }
+//navigate via a tag is not preferable
+//<a href="/">Navigate to page 1</a>
+//-> because: all data is gone
+//=> solution: use Link to prevent reloading
 
 const PageTwo = () => {
     return (
-        <div> page PageTwo
-            <button>Click Me</button>
+        <div> Page Two
+            <Link to="/">Navigate to page 1</Link>
         </div>
     )
 }
@@ -25,5 +31,9 @@ const App = () => {
         </div>
     )
 }
+
+//BrowserROuter: Uses everything after  TLD or Port
+//HashRouter: uses everything after a # as the path
+//MemoryRouter: dont use URL to track navigation
 
 export default App
