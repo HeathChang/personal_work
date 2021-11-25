1fae9b54145067ea8e7c595090e9a090aa759626
heathchang <jannhyunsoo@gmail.com>
2021-11-25 23:35:55 +0900

    211125_react@home_1

--- React/ModernReact/08_twitch/streams/client/src/components/App.js
+++ React/ModernReact/08_twitch/streams/client/src/components/App.js
@@ -1,31 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Route , Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamCreate from './streams/StreamCreate';
+import Header from './Header';
 
-const PageOne = () => {
-    return <div> page one
-        <Link to="/page/2">Navigate to page 2</Link>
-    </div>
-}
+// const PageOne = () => {
+//     return <div> page one
+//         <Link to="/page/2">Navigate to page 2</Link>
+//     </div>
+// }
 //navigate via a tag is not preferable
 //<a href="/">Navigate to page 1</a>
 //-> because: all data is gone
 //=> solution: use Link to prevent reloading
 
-const PageTwo = () => {
-    return (
-        <div> Page Two
-            <Link to="/">Navigate to page 1</Link>
-        </div>
-    )
-}
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/page/2" exact component={PageTwo} />
+                    <Header />
+
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" component={StreamCreate} />
+                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/delete" component={StreamDelete} />
+                    <Route path="/streams/show" component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
