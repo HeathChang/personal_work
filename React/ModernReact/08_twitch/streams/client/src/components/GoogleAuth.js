cdc572521420502a65f24aaebceba2acaa11de90
heathchang <jannhyunsoo@gmail.com>
2021-11-26 23:56:05 +0900

    211126_react@home_1

---
+++ React/ModernReact/08_twitch/streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component{
+    state = {isSignedIn: null}
+    componentDidMount(){
+        window.gapi.load('client:auth2',()=>{
+            window.gapi.client.init({
+                clientId: '634269985208-a75krn1rfsr662eviecgogobvsv0fn8r.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(()=>{
+                this.auth = window.gapi.auth2.getAuthInstance();
+                //getting a ref from auth instance
+                console.log(111,this.auth);
+                this.setState({isSignedIn: this.auth.isSignedIn.get()})
+
+            })
+        })
+        //load -> callBack && scope
+    }
+
+    renderAuthButton(){
+        if(this.state.isSignedIn === null){
+            return <div>I dnt knw if we are signed in</div>
+        } else if(this.state.isSignedIn){
+            return <div>I am Signed in</div>
+        } else{
+            <div> I am not Signed In </div>
+        }
+    }
+
+    render(){
+        return <div>{this.renderAuthButton()}</div>
+    }
+}
+
+export default GoogleAuth
