4264c286bf954e060cf0166c706d451a97c80faa
heathchang <jannhyunsoo@gmail.com>
2021-11-29 22:44:35 +0900

    211129_streamer_setup_1

--- React/ModernReact/08_twitch/streams/client/src/components/GoogleAuth.js
+++ React/ModernReact/08_twitch/streams/client/src/components/GoogleAuth.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-class GoogleAuth extends React.Component{
-    state = {isSignedIn: null}
-    componentDidMount(){
-        window.gapi.load('client:auth2',()=>{
-            window.gapi.client.init({
+class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
+  
+    componentDidMount() {
+      window.gapi.load('client:auth2', () => {
+        window.gapi.client
+          .init({
                 clientId: '634269985208-a75krn1rfsr662eviecgogobvsv0fn8r.apps.googleusercontent.com',
                 scope: 'email'
             }).then(()=>{
                 this.auth = window.gapi.auth2.getAuthInstance();
                 //getting a ref from auth instance
-                console.log(111,this.auth);
                 this.setState({isSignedIn: this.auth.isSignedIn.get()})
 
             })
@@ -24,7 +25,7 @@ class GoogleAuth extends React.Component{
         } else if(this.state.isSignedIn){
             return <div>I am Signed in</div>
         } else{
-            <div> I am not Signed In </div>
+            return <div> I am not Signed In </div>
         }
     }
 
--- React/ModernReact/08_twitch/streams/client/src/components/Header.js
+++ React/ModernReact/08_twitch/streams/client/src/components/Header.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import GoogleAuth
-    from './GoogleAuth';
-const Header = () => {
-    return (
-        <div className="ui secondary pointing menu">
-            <Link to="/" className="item">
-                Streamer
-            </Link>
-            <div className="right menu">
-                <Link to="/" className="item">
-                    All Streams
-                </Link>
-                <GoogleAuth />
-            </div>
-        </div>
-    )
-}
+import GoogleAuth from './GoogleAuth';
 
-export default Header
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamy
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+        <GoogleAuth />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
