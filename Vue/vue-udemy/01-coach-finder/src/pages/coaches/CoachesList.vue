c58d821d23bac8df2e851e2d06ae9430fef0deb8
heathchang <jannhyunsoo@gmail.com>
2022-01-11 00:22:04 +0900

    210111_1

---
+++ Vue/vue-udemy/01-coach-finder/src/pages/coaches/CoachesList.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <section>Filter</section>
+    <section>
+      <base-card>
+        <div class="controls">
+          <base-button>Refresh</base-button>
+          <base-button link to="/register">Register as Coach</base-button>
+        </div>
+        <ul v-if="hasCoaches">
+          <coach-item
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :id="coach.id"
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+          ></coach-item>
+        </ul>
+        <h3 v-else>No Coach found</h3>
+      </base-card>
+    </section>
+  </main>
+</template>
+
+<script>
+import CoachItem from "../../components/coaches/CouchItem.vue";
+import BaseButton from "../../components/ui/BaseButton.vue";
+export default {
+  components: {
+    CoachItem,
+    BaseButton,
+  },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters["coaches/coaches"];
+    },
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
