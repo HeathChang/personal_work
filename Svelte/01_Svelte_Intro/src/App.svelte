7924d44e22c1e06fcabeca20f80ad3f35bc7aeda
heathchang <jannhyunsoo@gmail.com>
2022-01-23 20:54:40 +0900

    210123_welcome_to_svelte_1

---
+++ Svelte/01_Svelte_Intro/src/App.svelte
@@ -0,0 +1,19 @@
+<h1>Hello {name.toUpperCase()}, My age is {age + 1}</h1>
+<button on:click="{incrementAge}"> Change Age</button>
+
+
+<script>
+	let name= 'heath';
+	let age= 30;
+
+	function incrementAge() {
+		age += 1
+	}
+</script>
+
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
