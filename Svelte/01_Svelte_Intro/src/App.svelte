f541c7b88b59b6162e12743f96b71f1e88d14c96
heathchang <jannhyunsoo@gmail.com>
2022-01-26 07:29:10 +0900

    210126_welcome_to_svelte_1

--- Svelte/01_Svelte_Intro/src/App.svelte
+++ Svelte/01_Svelte_Intro/src/App.svelte
@@ -1,13 +1,33 @@
-<h1>Hello {name.toUpperCase()}, My age is {age + 1}</h1>
+<h1>Hello {uppercaseName}, My age is {age + 1}</h1>
 <button on:click="{incrementAge}"> Change Age</button>
-
+<!-- <input type="text" value="{name}" on:input="{nameInput}"> -->
+<input type="text" bind:value="{name}" >
+<ConctactCard userName="{name}"/>
 
 <script>
+	import ConctactCard from './ConctactCard.svelte';
+
 	let name= 'heath';
 	let age= 30;
 
+	// add dynamic calculation: 
+	$: uppercaseName = name.toUpperCase();
+	$: console.log(name)
+	$: if (name === 'Jeffrey') { 
+		console.log('Hello')
+		age = 31
+	}
+	function nameInput(event){
+		const enteredValue = event.target.value
+		name = enteredValue
+	}
+
 	function incrementAge() {
 		age += 1
+	}
+	
+	function changeName() {
+		name = 'Jeffrey'
 	}
 </script>
 
