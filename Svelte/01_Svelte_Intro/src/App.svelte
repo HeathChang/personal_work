47b5f2ed0a4fc7c0b8dbac4e62f886170505b344
heathchang <jannhyunsoo@gmail.com>
2022-01-30 20:03:08 +0900

    210130_svelte_1

--- Svelte/01_Svelte_Intro/src/App.svelte
+++ Svelte/01_Svelte_Intro/src/App.svelte
@@ -8,19 +8,38 @@
   let formState = "empty";
 
   let createdContacts = [];
-  
-  function addContact () {
-    if(name.trim().length == 0 || title.trim().length == 0 || image.trim().length == 0 || description.length == 0) {formState = 'invalid'; return false}
-    createdContacts = [...createdContacts,({id:Math.random(),name: name , jobTitle: title , imageUrl: image , desc: description})]
-    // push 하면, reference type 인 createdContacts 에서는 변화가 되지 않는다. 
-    formState = 'done'
+
+  function addContact() {
+    // event.preventDefault();
+    if (
+      name.trim().length == 0 ||
+      title.trim().length == 0 ||
+      image.trim().length == 0 ||
+      description.trim().length == 0
+    ) {
+      formState = "invalid";
+      return;
+    }
+
+    createdContacts = [
+      ...createdContacts,
+      {
+        id: Math.random(),
+        name: name,
+        jobTitle: title,
+        imageUrl: image,
+        desc: description
+      }
+    ];
+    formState = "done";
   }
 
-  function deleteFirst () {
-    createdContacts = createdContacts.slice(1)
+  function deleteFirst() {
+    createdContacts = createdContacts.slice(1);
   }
-  function deleteLast () {
-    createdContacts = createdContacts.slice(0, -1)
+
+  function deleteLast() {
+    createdContacts = createdContacts.slice(0, -1);
   }
 </script>
 
@@ -28,10 +47,11 @@
   #form {
     width: 30rem;
     max-width: 100%;
+    margin: 1rem 0;
   }
 </style>
 
-<div id="form">
+<form id="form">
   <div class="form-control">
     <label for="userName">User Name</label>
     <input type="text" bind:value={name} id="userName" />
@@ -48,19 +68,28 @@
     <label for="desc">Description</label>
     <textarea rows="3" bind:value={description} id="desc" />
   </div>
-</div>
-<button on:click="{addContact}">Add Contact Card</button>
-<button on:click="{deleteFirst}">Delete First</button>
-<button on:click="{deleteLast}">Delete Last</button>
+  <button on:click|preventDefault={addContact} type="submit">Add Contact Card</button>
+</form>
+
+<button
+  on:click={event => createdContacts = createdContacts.slice(1)}>
+  Delete First
+</button>
+<button on:click={deleteLast}>Delete Last</button>
+
 {#if formState === 'invalid'}
-  <p> invalid input..</p>
+  <p>Invalid input.</p>
 {:else}
-  <p> Please enter some date and hit the button.</p>
+  <p>Please enter some data and hit the button!</p>
 {/if}
 
 {#each createdContacts as contact, i (contact.id)}
-<h2># {i + 1}</h2>
-<ContactCard userName={contact.name} jobTitle={contact.jobTitle} description={contact.desc} userImage={contact.imageUrl} />
+  <h2># {i + 1}</h2>
+  <ContactCard
+    userName={contact.name}
+    jobTitle={contact.jobTitle}
+    description={contact.desc}
+    userImage={contact.imageUrl} />
 {:else}
-  <p> Please start adding contents</p>
+  <p>Please start adding some contacts, we found none!</p>
 {/each}
