a867241f4a387ceaeb8e51b0e7e5e9fd1af2c108
heathchang <jannhyunsoo@gmail.com>
2022-03-16 00:17:29 +0900

    210316_React

--- React/CompleteGuide/01-starting-setup/src/App.js
+++ React/CompleteGuide/01-starting-setup/src/App.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense'
 
-const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ]
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+]
 
+const App = () => {
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   // return React.createElement(
   //   'div',
   //   {},
@@ -35,9 +37,13 @@ const App = () => {
   //1. which should be created
   //2. objects that configures
   //3. content between open and end
+
   const addExpenseHandler = (expense) => {
-    console.log('wwww: ', expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    });
   }
+
   return (
     <div>
       <h2>Let's get started!</h2>
--- React/CompleteGuide/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ React/CompleteGuide/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -7,24 +7,25 @@ const ExpenseItem = (props) => {
     //new Date()는 object이기 떄문에 toISOString() 사용해야함.
 
     //useState() is react hook
-    //const 사용하는 이유: = 사용해서 re-assign하지 못하게 하기 위해 
-    const [title, setTitle] = useState(props.title);
-    //1. point of the managed value => JSX code에서 사용할 이름 
+    //const 사용하는 이유: = 사용해서 re-assign하지 못하게 하기 위해
+    // const [title, setTitle] = useState(props.title);
+    //1. point of the managed value => JSX code에서 사용할 이름
     //2. function we can later call to set a new title
 
-    const clickHandler = () => {
-        setTitle('Updated ! ');
-    }
+    // const clickHandler = () => {
+    //     setTitle('Updated ! ');
+    // }
 
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title</button>
-        </Card>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date} />
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>
+            </Card>
+        </li>
     )
 }
 
