ef1d460263d4030cdfa4a52791bce9d3647bb692
heathchang <jannhyunsoo@gmail.com>
2022-03-19 14:42:12 +0900

    210319_React_2

--- React/CompleteGuide/06-PracticeProject/01-starting-project/src/components/Meals/MealItem/MealItemForm.jsx
+++ React/CompleteGuide/06-PracticeProject/01-starting-project/src/components/Meals/MealItem/MealItemForm.jsx
@@ -9,14 +9,14 @@ const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
       <Input
-        label='Amount'
+        label="Amount"
         input={{
-          id: 'amount_'+props.id,
+          id: 'amount_' + props.id, // this changed!
           type: 'number',
           min: '1',
           max: '5',
           step: '1',
-          defaultValue: '1'
+          defaultValue: '1',
         }}
       />
       <button> + ADD </button>
