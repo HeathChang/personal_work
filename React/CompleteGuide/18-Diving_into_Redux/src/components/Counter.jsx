f99faf23a0cfb1be078c8f130865ed71d09d4de0
heathchang <jannhyunsoo@gmail.com>
2022-03-27 15:17:41 +0900

    react_redux_1

--- React/CompleteGuide/18-Diving_into_Redux/src/components/Counter.jsx
+++ React/CompleteGuide/18-Diving_into_Redux/src/components/Counter.jsx
@@ -1,31 +1,35 @@
-import { useSelector , useDispatch , connect } from 'react-redux'
+import { useSelector, useDispatch, connect } from 'react-redux'
 import classes from './css/Counter.module.css';
 
+import { counterActionsCreator } from "../store";
 
 const Counter = () => {
 	// 4. useSelector : automatically select the part of the state managed by store
 	// 4-2. by using useSelector, Redux will automatically set up a subscription. so this will receive the latest counter automatically when data changes in Redux store.
 
-	const counter = useSelector ( state => state.counter )
-	const showCounter = useSelector ( state => state.showCounter )
+	const counter = useSelector(state => state.counter)
+	const showCounter = useSelector(state => state.showCounter)
 
 	// 5. dispatch: dispatch an action to the Redux store
-	const dispatch = useDispatch ();
+	const dispatch = useDispatch();
 	const incrementDispatcher = () => {
 		// 5.2 dispatch an action
-		dispatch ( { type : 'increment' } )
+		// dispatch({ type : 'increment' })
+		dispatch(counterActionsCreator.increment())
 	}
 	const incrementDispatcherByAmount = () => {
 		// 5.2 dispatch an action
-		dispatch ( { type : 'increment' , amount : 5 } )
+		// dispatch({ type : 'increment', amount : 5 })
+		dispatch(counterActionsCreator.increment({amount: 5}))
 	}
 	const decrementDispatcher = () => {
-		dispatch ( { type : 'decrement' } )
+		// dispatch({ type : 'decrement' })
+		dispatch(counterActionsCreator.decrement())
 	}
-
-
 	const toggleCounterHandler = () => {
-		dispatch ( { type : 'toggle' } )
+		// dispatch({ type : 'toggle' })
+		dispatch(counterActionsCreator.toggleCounter())
+
 
 	};
 // 6. connect: wrapper around our class Component to connect that class component
@@ -33,7 +37,7 @@ const Counter = () => {
 	return (
 			<main className={ classes.counter }>
 				<h1>Redux Counter</h1>
-				{showCounter && <div className={ classes.value }>{ counter }</div>}
+				{ showCounter && <div className={ classes.value }>{ counter }</div> }
 				<div>
 					<button onClick={ incrementDispatcher }>Increment</button>
 					<button onClick={ incrementDispatcherByAmount }>Increment by 5</button>
--- React/CompleteGuide/18-Diving_into_Redux/src/store/index.js
+++ React/CompleteGuide/18-Diving_into_Redux/src/store/index.js
@@ -1,67 +1,72 @@
 // import { createStore } from 'redux';
-import { createSlice , configureStore } from '@reduxjs/toolkit'
+import { createSlice, configureStore } from '@reduxjs/toolkit'
 
 
-const initialState = { counter : 0 , showCounter : true }
+const initialState = { counter : 0, showCounter : true }
 
-const counterSlice = createSlice ( {
-	name : 'counter' , initialState : initialState , reducers : {
-		increment ( state , { type , amount = 1 } ) {
+const counterSlice = createSlice({
+	name : 'counter', initialState : initialState, reducers : {
+		// if duplicated destructured value  is null, put amount : 1
+		increment (state, { payload : { amount } = { amount : 1 } }) {
 			//immutable: automatically over-riding
 			// state.counter++
+			console.log('check payload:: ', amount)
 			state.counter = state.counter + amount
-		} , decrement ( state , { type , amount = 1 } ) {
+		},
+		decrement (state, { amount = 1 }) {
 			state.counter = state.counter - amount
-		} , toggleCounter ( state ) {
+		},
+		toggleCounter (state) {
 			state.showCounter = !state.showCounter
 		}
 	}
-} )
+})
 
 //////////////////////////////////////////////////////////////
 //1. CreateStore & reducer and  connect createStore to reducer
 // const store = createStore()
 
 //2. create reducer and mutates the state
-const counterReducer = ( state = initialState , { type , amount = 1 } ) => {
+const counterReducer = (state = initialState, { type, amount = 1 }) => {
 	//receive action from component by dispatching
 	if ( type === 'increment' ) {
 		// why do we have to return state ->  never mutate the state, but overWrite it !!
 		return {
 			//quiet inconvenient
-			counter : state.counter + amount , showCounter : state.showCounter
+			counter : state.counter + amount, showCounter : state.showCounter
 		}
 	}
 	if ( type === 'decrement' ) {
 		return {
 			//quiet inconvenient
-			counter : state.counter - amount , showCounter : state.showCounter
+			counter : state.counter - amount, showCounter : state.showCounter
 		}
 	}
 
 	if ( type === 'toggle' ) {
 		return {
 			//quiet inconvenient
-			counter : state.counter , showCounter : !state.showCounter
+			counter : state.counter, showCounter : !state.showCounter
 		}
 	}
 
 	return state;
 }
 
+// create [action creator methods] to creat action and dispatch an action(+ unique id)
 
-// $$$$$$ changes from
+// $$$$$$ (createStore) changes from
 // const store = createStore ( counterReducer )
 // $$$$$ changes to
 // const store = createStore ( counterSlice.reducer )
 // $$$$$ changes to
-const store = configureStore ( {
+const store = configureStore({
 	// must have single reducer
 	reducer : counterSlice.reducer
 	// if many,
 	// reducer : { a: counterSlice.reducer, b: counterSlice.reducer...}
-} )
+})
 
 
-//3. from other files and provide store to other file (firstly to index.js)
+export const counterActionsCreator = counterSlice.actions;
 export default store;
