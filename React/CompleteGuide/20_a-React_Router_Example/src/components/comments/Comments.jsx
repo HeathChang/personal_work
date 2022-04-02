df80fcf93416724233c21d7b034a67c282cd1709
heathchang <jannhyunsoo@gmail.com>
2022-04-02 14:03:25 +0900

    290: error fix

--- React/CompleteGuide/20_a-React_Router_Example/src/components/comments/Comments.jsx
+++ React/CompleteGuide/20_a-React_Router_Example/src/components/comments/Comments.jsx
@@ -11,8 +11,9 @@ import CommentsList from "./CommentsList";
 const Comments = () => {
 	const [ isAddingComment, setIsAddingComment ] = useState(false);
 	const params = useParams()
+	console.log('params check:: ', params)
 	const { quoteId } = params
-	const { sendRequest, status, data : loadedComments, error } = useHttp(getAllComments);
+	const { sendRequest, status, data : loadedComments } = useHttp(getAllComments);
 
 	const startAddCommentHandler = () => {
 		setIsAddingComment(true);
@@ -27,9 +28,9 @@ const Comments = () => {
 
 	let comments;
 	if ( status === 'pending' ) {
-		comments = <p className="centered"><LoadingSpinner/></p>
+		comments = <div className="centered"><LoadingSpinner/></div>
 	} else if ( status === 'completed' && ( !loadedComments || loadedComments.length === 0 ) ) {
-		comments = <p className="centered">No Comment .. </p>
+		comments = <div className="centered">No Comment .. </div>
 	} else if ( status === 'completed' ) {
 		comments = <CommentsList comments={ loadedComments }/>
 	}
@@ -42,7 +43,7 @@ const Comments = () => {
 						</button>
 				) }
 				{ isAddingComment && <NewCommentForm quoteId={ quoteId } onAddedComment={ addedCommentHandler }/> }
-				<p>{ comments }</p>
+				<div>{ comments }</div>
 			</section>
 	);
 };
--- React/CompleteGuide/20_a-React_Router_Example/src/components/comments/NewCommentForm.jsx
+++ React/CompleteGuide/20_a-React_Router_Example/src/components/comments/NewCommentForm.jsx
@@ -1,33 +1,29 @@
 import { useRef, useEffect } from 'react';
 import { addComment } from '../../lib/api.js'
-import useHttp from '../../hooks/use-http'
 import classes from './NewCommentForm.module.css';
 import useHttps from "../../hooks/use-http";
-import { useHistory, useParams } from "react-router-dom";
 import LoadingSpinner from "../UI/LoadingSpinner";
 
 const NewCommentForm = (props) => {
 	const commentTextRef = useRef();
 	const { sendRequest, status, error } = useHttps(addComment);
-	const history = useHistory();
-
 	const { onAddedComment } = props
+
 	useEffect(() => {
 		if ( status === 'completed' && !error ) {
 			onAddedComment();
-			// history.replace(`/quotes/${ params.quoteId }`)
 		}
 	}, [ status, error, onAddedComment ])
+
 	const submitFormHandler = (event) => {
 		event.preventDefault();
-		const enteredText = commentTextRef.current.value
-		sendRequest({ commentData : { text : enteredText }, quoteId : props.quoteId })
+		sendRequest({ commentData : { text : commentTextRef.current.value }, quoteId : props.quoteId })
 		// send comment to server
 	};
 
 	return (
 			<form className={ classes.form } onSubmit={ submitFormHandler }>
-				{ status === 'pending' && <p className='centered'><LoadingSpinner/></p> }
+				{ status === 'pending' && <div className='centered'><LoadingSpinner/></div> }
 				<div className={ classes.control } onSubmit={ submitFormHandler }>
 					<label htmlFor='comment'>Your Comment</label>
 					<textarea id='comment' rows='5' ref={ commentTextRef }></textarea>
