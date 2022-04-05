62b4a8c348349a1a370ead2dc761909b502d34e1
heathchang <jannhyunsoo@gmail.com>
2022-04-05 23:42:20 +0900

    323: next_initial

--- React/CompleteGuide/21_Authentication/src/store/auth-context.js
+++ React/CompleteGuide/21_Authentication/src/store/auth-context.js
@@ -66,6 +66,7 @@ export const AuthContextProvider = (props) => {
 			logoutTimer = setTimeout(logoutHandler, tokenData.duration)
 		}
 	}, [ tokenData, logoutHandler ])
+
 	const contextValue = {
 		token : token,
 		isLoggedIn : userIsLoggedIn,
---
+++ React/CompleteGuide/22_Next/pages/news.jsx
@@ -0,0 +1,7 @@
+// request to our-domain/news// root page (our-domain/)
+
+function NewsPage(){
+	return <h1>The News Page</h1>
+}
+
+export default NewsPage
