85d0b4ecfb569e046a62fb52b556bcb39882b3a4
heathchang <jannhyunsoo@gmail.com>
2022-04-07 23:19:08 +0900

    Project- geolocation 을 통한 경도/위도 가져와서 카카오맵에 적용

--- Project/01_FooNea/src/Components/Main/map.jsx
+++ Project/01_FooNea/src/Components/Main/map.jsx
@@ -1,14 +1,32 @@
 import classes from './css/index.module.css'
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+
 const { kakao } = window;
+
 
 const Map = () => {
 	//함수형 컴포넌트에서는 kakao script를 인지불가. const { kakao } = window를 통해 함수형 컴포넌트에 인지 시킨다고 합니다.
 	// https://velog.io/@lifeisbeautiful/React%EC%B9%B4%EC%B9%B4%EC%98%A4-API-%EC%A0%81%EC%9A%A9
-	useEffect(()=>{
+
+	const [ lat, setLat ] = useState(null)
+	const [ long, setLong ] = useState(null)
+
+	useEffect(() => {
+		window.navigator.geolocation.getCurrentPosition(
+				(position) => {
+					setLat(position.coords.latitude)
+					setLong(position.coords.longitude)
+				},
+				(error) => {
+					console.log(error)
+				}
+		);
+	})
+
+	useEffect(() => {
 		const container = document.getElementById('myMap');
 		const options = {
-			center : new kakao.maps.LatLng(33.450701, 126.570667),
+			center : new kakao.maps.LatLng(lat, long),
 			level : 3
 		};
 		const map = new kakao.maps.Map(container, options);
@@ -16,7 +34,10 @@ const Map = () => {
 
 
 	return (
-			<div className={ classes.map } id="myMap"></div>
+			<Fragment>
+				<h1>Lat: { lat } * Long: { long }</h1>
+				<div className={ classes.map } id="myMap"></div>
+			</Fragment>
 	)
 }
 
