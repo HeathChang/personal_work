f1e90562a19d89bf8db1d7d0620fd56662021748
heathchang <jannhyunsoo@gmail.com>
2022-04-07 22:57:35 +0900

    Project- .env를 통해 api키 감추기

--- Project/01_FooNea/src/Components/Main/map.jsx
+++ Project/01_FooNea/src/Components/Main/map.jsx
@@ -4,8 +4,8 @@ const { kakao } = window;
 
 const Map = () => {
 	//함수형 컴포넌트에서는 kakao script를 인지불가. const { kakao } = window를 통해 함수형 컴포넌트에 인지 시킨다고 합니다.
+	// https://velog.io/@lifeisbeautiful/React%EC%B9%B4%EC%B9%B4%EC%98%A4-API-%EC%A0%81%EC%9A%A9
 	useEffect(()=>{
-		// const container = document.getElementsByClassName('map');
 		const container = document.getElementById('myMap');
 		const options = {
 			center : new kakao.maps.LatLng(33.450701, 126.570667),
