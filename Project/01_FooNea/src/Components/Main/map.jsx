d6e3d0b01a0cf2b52a4e9fed4cc4cdc9b57db177
heathchang <jannhyunsoo@gmail.com>
2022-04-19 08:01:53 +0900

    Memo insert

--- Project/01_FooNea/src/Components/Main/map.jsx
+++ Project/01_FooNea/src/Components/Main/map.jsx
@@ -36,7 +36,6 @@ const Map = () => {
 		const pinImage = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png'
 
 		data.map(toilet => {
-			console.log(11, new kakao.maps.LatLng(toilet.lat, toilet.long))
 			// 마커 이미지 크기
 			let imageSize = new kakao.maps.Size(24, 35);
 			// 마커 이미지 생성
@@ -49,7 +48,6 @@ const Map = () => {
 				title : toilet.name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
 				image : markerImage // 마커 이미지
 			});
-			console.log('toilet check:: ', marker)
 		})
 
 	})
--- Project/01_FooNea/src/Components/Memo/index.jsx
+++ Project/01_FooNea/src/Components/Memo/index.jsx
@@ -1,12 +1,24 @@
-import classes from "../Main/css/index.module.css";
+import classes from "./css/index.module.css";
 import { Fragment } from "react";
 
 const test = () => {
+
+	const fnInsertMemo = () => {
+		console.log('Insert Memo')
+	}
+
 	return (
 			<Fragment>
 				<h1 className={ classes.title }>
 					Memo
 				</h1>
+				<div>
+					<ul>
+						<li className={classes.list}>
+							<textarea className={classes.textarea} placeholder="insert memo" onKeyPress={(e)=>{if(e.key ==='Enter'){ fnInsertMemo()}}}/>
+						</li>
+					</ul>
+				</div>
 			</Fragment>
 	)
 }
