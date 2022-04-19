e0741e38aae6aef89d554c9064eee37b126e5256
heathchang <jannhyunsoo@gmail.com>
2022-04-20 00:58:00 +0900

    dont forget useRef

--- Project/01_FooNea/src/Components/Memo/index.jsx
+++ Project/01_FooNea/src/Components/Memo/index.jsx
@@ -1,26 +1,29 @@
 import classes from "./css/index.module.css";
 import { Fragment } from "react";
+import { useRef, useState } from 'react';
 
-const test = () => {
+const Memo = () => {
+	const textInputRef = useRef();
 
-	const fnInsertMemo = () => {
-		console.log('Insert Memo')
+	function fnInsertMemo() {
+		console.log(112, textInputRef)
+		const enteredText = textInputRef.current.value;
 	}
 
-	return (
-			<Fragment>
-				<h1 className={ classes.title }>
-					Memo
-				</h1>
-				<div>
-					<ul>
-						<li className={classes.list}>
-							<textarea className={classes.textarea} placeholder="insert memo" onKeyPress={(e)=>{if(e.key ==='Enter'){ fnInsertMemo()}}}/>
-						</li>
-					</ul>
-				</div>
-			</Fragment>
-	)
+	return ( <Fragment>
+		<h1 className={ classes.title }>
+			Memo
+		</h1>
+		<div>
+			<div className={ classes.title }>
+						<textarea className={ classes.textarea } ref={ textInputRef } placeholder="insert memo" onKeyPress={ (e) => {
+							if ( e.key === 'Enter' ) {
+								fnInsertMemo()
+							}
+						} }/>
+			</div>
+		</div>
+	</Fragment> )
 }
 
-export default test
+export default Memo
