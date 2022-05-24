a89e992ef050ff3a36ed171f7be7be123b12e593
heathchang <jannhyunsoo@gmail.com>
2022-05-24 23:56:38 +0900

    SQLite_start2

--- NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/users/users.service.ts
+++ NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/users/users.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {Repository} from 'typeorm';
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private repo: Repository<User>) {
+        // 1.repo : argument name ( + abv )
+        // 2.Repository<User>: repo is an instance of a type ORM repository that deals with instance of Users
+        // 3.@InjectRepository: Aid to DI system which will tell the DI system that we need the Repository<User> repository
+    }
+
+    create(email: string, password: string) {
+        const user = this.repo.create({email, password});
+        return this.repo.save(user)
+    }
+}
