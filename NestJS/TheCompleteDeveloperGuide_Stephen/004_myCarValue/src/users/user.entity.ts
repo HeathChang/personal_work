5ffaca4d9645a9638edb845af126da40d9343e2e
heathchang <jannhyunsoo@gmail.com>
2022-05-25 23:49:22 +0900

    SQLite_start3

--- NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/users/user.entity.ts
+++ NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/users/user.entity.ts
@@ -1,11 +1,29 @@
-import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
+import {Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove} from "typeorm";
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
+
     @Column()
     email: string;
+
     @Column()
     password: string;
+
+    @AfterInsert()
+    logInsert(){
+        console.log('after insert:: ',this.id)
+    }
+
+    @AfterUpdate()
+    logUpdate(){
+        console.log('after update:: ', this.id)
+    }
+
+    @AfterRemove()
+    logRemove(){
+        console.log('remove:: ', this.id)
+    }
+
 }
--- NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/users/users.service.ts
+++ NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/users/users.service.ts
@@ -13,6 +13,39 @@ export class UsersService {
 
     create(email: string, password: string) {
         const user = this.repo.create({email, password});
+        //by using create => able to use hooks (logInsert etc..)
         return this.repo.save(user)
+    }
+
+    findOne(id: number) {
+        // @ts-ignore
+        return this.repo.findOne(id);
+    }
+
+    find(email: string) {
+        // @ts-ignore
+        return this.repo.find({email});
+    }
+
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new Error('user not found');
+        }
+        console.log('attr check:: ', attrs)
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
+    async remove(id: number, attrs: Partial<User>) {
+        // @ts-ignore
+        const user = await this.findOne({email})
+        if (!user) {
+            throw new Error('user not found');
+        }
+        console.log('attr check:: ', attrs)
+        Object.assign(user, attrs)
+        return this.repo.remove(user)
     }
 }
