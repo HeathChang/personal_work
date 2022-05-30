8db7c4b0b9c8901bb79b0a337b5dde5d3ed7b398
heathchang <jannhyunsoo@gmail.com>
2022-05-30 23:34:26 +0900

    TDD_2

--- NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/users/auth.service.spec.ts
+++ NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/users/auth.service.spec.ts
@@ -2,12 +2,18 @@ import {Test} from '@nestjs/testing';
 import {create} from 'domain';
 import {AuthService} from "./auth.service";
 import {UsersService} from "./users.service";
+import {User} from "./user.entity";
 
 it('can create an instance of authService', async () => {
     //Create a fake copy of the users service
-    const fakeUsersService = {
+    // Partial: TS will just check the properties we use.
+    const fakeUsersService: Partial<UsersService> = {
+        // Why do we only create find & create => AuthService only use those two
+
+        // Promise.resolve(): find & create is asynchronous, so need time to read & write data to DB => need to have promise from two methods
+        // Promise.resolve creates Promise and immediately resolve it
         find: () => Promise.resolve([]),
-        create: (email: string, password: string) => Promise.resolve({id: 1, email, password}),
+        create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User),
     }
 
     const module = await Test.createTestingModule({
