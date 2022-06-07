ea998f73484f8ea82a4e20a8df45382b5368e509
heathchang <jannhyunsoo@gmail.com>
2022-06-07 23:14:14 +0900

    intro

--- NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/guards/auth.guard.ts
+++ NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/guards/auth.guard.ts
@@ -5,7 +5,7 @@ import {
 
 
 export class AuthGuard implements CanActivate {
-    //CanActivate(): Return truthy value if user cann access to the route, false if not.
+    //CanActivate(): Return truthy value if user can access to the route, false if not.
     canActivate(context: ExecutionContext) {
         const request = context.switchToHttp().getRequest()
         return request.session.userId
