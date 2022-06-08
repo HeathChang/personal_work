dd21302d08dc30b5771476bf0819ea97382be27a
heathchang <jannhyunsoo@gmail.com>
2022-06-08 20:14:46 +0900

    intro_3

--- NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/users/user.entity.ts
+++ NestJS/TheCompleteDeveloperGuide_Stephen/004_myCarValue/src/users/user.entity.ts
@@ -1,28 +1,32 @@
-import {Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove} from "typeorm";
+import {Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove, OneToMany} from "typeorm";
+import {Report} from '../reports/report.entity'
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
-    
+
     @Column()
     email: string;
 
     @Column()
     password: string;
 
+    @OneToMany(() => Report, (report) => report.user)
+    reports: Report[];
+
     @AfterInsert()
-    logInsert(){
-        console.log('after insert:: ',this.id)
+    logInsert() {
+        console.log('after insert:: ', this.id)
     }
 
     @AfterUpdate()
-    logUpdate(){
+    logUpdate() {
         console.log('after update:: ', this.id)
     }
 
     @AfterRemove()
-    logRemove(){
+    logRemove() {
         console.log('remove:: ', this.id)
     }
 
