bc00096ba578458815fee9f253e9651e08cf9e7c
heathchang <jannhyunsoo@gmail.com>
2022-06-08 21:16:56 +0900

    basic setup_2

--- Project/04_mbti/src/components/common/TheHeader.vue
+++ Project/04_mbti/src/components/common/TheHeader.vue
@@ -3,5 +3,41 @@
 </template>
 
 <script>
+import { computed, getCurrentInstance, reactive, toRef, toRefs } from "vue";
+import { useStore } from 'vuex'
+import { modalNames } from '../globalComponent'
 
+const layoutState = () => {
+  const { getters } = useStore()
+  const routeLayout = computed(() => {
+    return getters['layout/getRouteLayout']
+  })
+
+  return {
+    routeLayout
+  }
+}
+
+const methodState = () => {
+
+
+  return {}
+}
+
+export default {
+  name : 'the-header',
+  setup() {
+    const page = reactive({
+      'test' : 1
+    })
+
+    return {
+      ...toRefs(page),
+      ...layoutState(),
+      ...methodState()
+    }
+  }
+
+
+}
 </script>
---
+++ Project/04_mbti/src/store/layout/index.js
@@ -0,0 +1,76 @@
+const routeState = {
+	isHeader : false,
+	titleName : '',
+	isBack : false,
+	isNav : false,
+	isFooter : false
+}
+
+export const state = {
+	isLoading : false,
+	modalComponents : [],
+	isFocus : false,
+	routeLayout : {
+		...routeState
+	}
+}
+
+export const getters = {
+	getIsLoading : state => state.isLoading,
+	getModalComponents : state => state.modalComponents,
+	getIsFocus : state => state.isFocus,
+	getRouteLayout : state => state.routeLayout
+}
+
+export const mutations = {
+	setIsLoading(state, { isLoading }) {
+		state.isLoading = isLoading
+	},
+	setModalComponents(state, { modalComponents }) {
+		state.modalComponents = modalComponents
+	},
+	setIsFocus(state, { isFocus = false }) {
+		state.isFocus = isFocus
+	},
+	setRouteLayout(state, {
+		isHeader = undefined,
+		titleName = undefined,
+		isBack = undefined,
+		isNav = undefined,
+		isFooter = undefined
+	}) {
+		if ( isHeader !== undefined ) state.routeLayout = isHeader
+		if ( titleName !== undefined ) state.titleName = titleName
+		if ( isBack !== undefined ) state.isBack = isBack
+		if ( isNav !== undefined ) state.isNav = isNav
+		if ( isFooter !== undefined ) state.isFooter = isFooter
+	}
+}
+
+export const actions = {
+	updateIsLoading({ commit }, { isLoading = false }) {
+		commit('setIsLoading', { isLoading })
+	},
+	updateModalComponent({ state, commit }, payload) {
+		state.modalComponents.push(payload)
+		commit('setModalComponent', {
+			modalComponents : state.modalComponents
+		})
+	},
+	removeModalComponent({ state, commit }, { index = 0 }) {
+		state.modalComponents.splice(index, 1)
+		commit('setModalComponent', {
+			modalComponents : state.modalComponents
+		})
+	},
+	updateRouteLayout({ commit }, payload) {
+		commit('setRouteLayout', payload)
+	}
+}
+
+export default {
+	namespaced : true,
+	state, getters, mutations, actions
+}
+
+
