2eabbeb1c9c8722c301f28d63afb287eb0f7ca40
heathchang <jannhyunsoo@gmail.com>
2022-06-15 21:52:24 +0900

    beginning page_4

--- Project/04_mbti/src/views/test/index.vue
+++ Project/04_mbti/src/views/test/index.vue
@@ -3,7 +3,12 @@
     <div class="main_inner">
       <div class="page">
         <test-step1 v-if="step === '1'" @done="fnNext('2')"/>
-        <test-step2 v-if="step === '2'" @done="fnNext('3')"/>
+        <test-step2 v-if="step === '2'" @done="fnNext('3')" :step="parseInt(step)+1"/>
+        <test-step3 v-if="step === '3'" @done="fnNext('4')" :step="parseInt(step)+1"/>
+        <test-step4 v-if="step === '4'" @done="fnNext('5')" :step="parseInt(step)+1"/>
+        <test-step5 v-if="step === '5'" @done="fnNext('done')" :step="'done'"/>
+
+        <div v-if="step === 'done'">끝입니다</div>
       </div>
     </div>
   </div>
@@ -12,11 +17,14 @@
 <script>
 import TestStep1 from './step1'
 import TestStep2 from './step2'
+import TestStep3 from './step2'
+import TestStep4 from './step2'
+import TestStep5 from './step2'
 import { onMounted, reactive, toRefs } from "vue";
 
 export default {
   name : "test-index",
-  components : { TestStep1, TestStep2 },
+  components : { TestStep1, TestStep2, TestStep3, TestStep4, TestStep5 },
   setup() {
     const state = reactive({
       step : '1',
@@ -24,13 +32,14 @@ export default {
 
     const fnNext = (index) => {
       state.step = index
+
     }
 
     onMounted(() => {
-      if(state.step !== (sessionStorage.getItem('step')||'1')){
+      if ( state.step !== ( sessionStorage.getItem('step') || '1' ) && state.step !== 'done' ) {
         alert('이전진행단계부터 진행합니다..')
       }
-      state.step = (sessionStorage.getItem('step') ||'1')
+      state.step = ( sessionStorage.getItem('step') || '1' )
     })
 
     return {
--- Project/04_mbti/src/views/test/step2.vue
+++ Project/04_mbti/src/views/test/step2.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="inner-container">
+    {{ step }}
     <div class="inner-main-question" v-for="(item,index) in dummy" :key="index">
       <div class="inner-main-title">
         {{ index }}. {{ item.q }}
       </div>
-      <ul class="main-select">
-        <li v-for="(item2,index2) in item.r" :key="index2">
+      <ul class="main-select" id="step2-select-ul">
+        <li id="step2-select-li" v-for="(item2,index2) in item.r" :key="index2">
           <input
               type="radio"
               name="`${item2.id}_${index2}`"
@@ -17,24 +18,37 @@
         </li>
       </ul>
     </div>
+    <div class="confirm">
+      <button @click.prevent.stop="fnConfirm(step)">Next :: {{ step -1 }}</button>
+    </div>
   </div>
 </template>
 
 
 <script>
 import data from '@/dummy/data'
-import { reactive, toRefs } from "vue";
+import { getCurrentInstance, reactive, toRefs } from "vue";
 
 export default {
   name : 'test-step2',
-  setup() {
-
+  props: {
+    step: String || Number
+  },
+  setup(props) {
+    const {proxy} = getCurrentInstance()
     const state = reactive({
       dummy : data.step1
     })
 
+    const fnConfirm = (index) => {
+      sessionStorage.setItem('step', index)
+      setTimeout(()=>{
+        proxy.$emit('done')
+      },250)
+    }
     return {
-      ...toRefs(state)
+      ...toRefs(state),
+      fnConfirm
     }
   }
 }
