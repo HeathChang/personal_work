baac97d4f472ca444957ff06aa95550eaeb40892
heathchang <jannhyunsoo@gmail.com>
2022-06-16 22:02:45 +0900

    beginning page_4

--- Project/04_mbti/src/views/test/step2.vue
+++ Project/04_mbti/src/views/test/step2.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="inner-container">
-    {{ step }}
     <div class="inner-main-question" v-for="(item,index) in dummy" :key="index">
       <div class="inner-main-title">
         {{ index }}. {{ item.q }}
@@ -11,6 +10,7 @@
               type="radio"
               name="`${item2.id}_${index2}`"
               id="`${item2.id}_${index2}`"
+              @click="fnAdd(item.q,item2)"
           />
           <label for="`${item2.id}_${index2}`" class="select-text">
             <p>{{ item2 }}</p>
@@ -19,7 +19,7 @@
       </ul>
     </div>
     <div class="confirm">
-      <button @click.prevent.stop="fnConfirm(step)">Next :: {{ step -1 }}</button>
+      <button @click.prevent.stop="fnConfirm(step)">Next :: {{ step - 1 }}</button>
     </div>
   </div>
 </template>
@@ -31,24 +31,33 @@ import { getCurrentInstance, reactive, toRefs } from "vue";
 
 export default {
   name : 'test-step2',
-  props: {
-    step: String || Number
+  props : {
+    step : String || Number
   },
   setup(props) {
-    const {proxy} = getCurrentInstance()
+    const { proxy } = getCurrentInstance()
     const state = reactive({
       dummy : data.step1
+      // dummy : `data.step${ props.step }`
+
     })
 
+    const fnAdd = (index, value) => {
+      console.log(index, value)
+    }
     const fnConfirm = (index) => {
-      sessionStorage.setItem('step', index)
-      setTimeout(()=>{
+      // sessionStorage.setItem('step', props.step)
+
+
+      return false
+      setTimeout(() => {
         proxy.$emit('done')
-      },250)
+      }, 250)
     }
     return {
       ...toRefs(state),
-      fnConfirm
+      fnConfirm,
+      fnAdd
     }
   }
 }
