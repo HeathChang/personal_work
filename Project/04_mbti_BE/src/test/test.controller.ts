e921b497a6224f4785b6c533df8139fe45ce319b
heathchang <jannhyunsoo@gmail.com>
2022-06-23 21:47:17 +0900

    22

--- Project/04_mbti_BE/src/test/test.controller.ts
+++ Project/04_mbti_BE/src/test/test.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Param } from "@nestjs/common";
 import { TestDto as TestDTO } from "../dtos/test.dto";
 
-@Controller("/test")
+@Controller("test")
 export class TestController {
   @Post("/postTest")
   saveTestResult(@Param() params: any) {
