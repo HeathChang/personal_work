bbd7ce66714bcd0313c0fa04be7658fa1b888b79
heathchang <jannhyunsoo@gmail.com>
2022-07-03 19:34:53 +0900

    typeorm_2

--- Project/04_mbti_BE/src/configs/mbti_typeorm.config.ts
+++ Project/04_mbti_BE/src/configs/mbti_typeorm.config.ts
@@ -1,13 +1,16 @@
-import {TypeOrmModuleOptions} from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { MBTI } from "../test/entity/mbti.entity";
+
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: 'localhost',
+  type: "mysql",
+  host: "localhost",
   port: 3306,
-  username: 'root',
-  password: '1234',
-  database:'mbti',
-  entities: ['dist/**/*.entity.ts'],
-  synchronize: false
+  username: "root",
+  password: "1234",
+  database: "mbti",
+  entities: [MBTI],
+  // entities: ['dist/**/*.entity.ts'],
+  synchronize: true
 
-}
+};
---
+++ Project/04_mbti_BE/src/test/entity/mbti.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class MBTI {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ comment: "인덱스" })
+  index: number;
+
+
+  @Column({ comment: "질문" })
+  question: String;
+
+  @Column({ comment: "대답" })
+  response: String;
+}
