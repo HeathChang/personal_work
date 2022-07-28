932d7f5b6b683f4c1928b8a9e6ab57a9ec343c78
heathchang <jannhyunsoo@gmail.com>
2022-07-28 21:38:13 +0900

    docker

--- NestJS/TheCompleteDeveloperGuide_Stephen/002_messages_cli/src/messages/messages.controller.ts
+++ NestJS/TheCompleteDeveloperGuide_Stephen/002_messages_cli/src/messages/messages.controller.ts
@@ -1,34 +1,34 @@
-import {Body, Controller, Get, NotFoundException, Param, Post} from '@nestjs/common';
-import {CreateMessageDTO} from './DTO/create-message.dto';
-import {MessagesService} from "./messages.service";
-
-@Controller('messages')
-export class MessagesController {
-    // messagesService: MessagesService;
-    //
-    // constructor() {
-    //     this.messagesService = new MessagesService();
-    // }
-
-    constructor(public messagesService: MessagesService) {
-    }
-
-    @Get()
-    listMessages() {
-        return this.messagesService.findAll();
-    }
-
-    @Post()
-    createMessage(@Body() body: CreateMessageDTO) {
-        return this.messagesService.create(body.content)
-    }
-
-    @Get('/:id')
-    async getMessage(@Param('id') id: string) {
-        const message = await this.messagesService.findOne(id)
-        if (!message) {
-            throw new NotFoundException('Message not Found ')
-        }
-        return message
-    }
-}
+// import {Body, Controller, Get, NotFoundException, Param, Post} from '@nestjs/common';
+// import {CreateMessageDTO} from './DTO/create-message.dto';
+// import {MessagesService} from "./messages.service";
+//
+// @Controller('messages')
+// export class MessagesController {
+//     // messagesService: MessagesService;
+//     //
+//     // constructor() {
+//     //     this.messagesService = new MessagesService();
+//     // }
+//
+//     constructor(public messagesService: MessagesService) {
+//     }
+//
+//     @Get()
+//     listMessages() {
+//         return this.messagesService.findAll();
+//     }
+//
+//     @Post()
+//     createMessage(@Body() body: CreateMessageDTO) {
+//         return this.messagesService.create(body.content)
+//     }
+//
+//     @Get('/:id')
+//     async getMessage(@Param('id') id: string) {
+//         const message = await this.messagesService.findOne(id)
+//         if (!message) {
+//             throw new NotFoundException('Message not Found ')
+//         }
+//         return message
+//     }
+// }
