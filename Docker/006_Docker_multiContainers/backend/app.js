470710df9ba071669d04ec2d056c4a5fc03d32a7
heathchang <jannhyunsoo@gmail.com>
2022-08-23 23:58:05 +0900

    docker_frontend_dockerize_feat_network_2_1

--- Docker/006_Docker_multiContainers/backend/app.js
+++ Docker/006_Docker_multiContainers/backend/app.js
@@ -62,7 +62,7 @@ app.post('/goals', async (req, res) => {
     res
       .status(201)
       .json({ message: 'Goal saved', goal: { id: goal.id, text: goalText } });
-    console.log('STORED NEW GOAL');
+    console.log('STORED NEW GOAL~');
   } catch (err) {
     console.error('ERROR FETCHING GOALS');
     console.error(err.message);
@@ -86,17 +86,17 @@ app.delete('/goals/:id', async (req, res) => {
 mongoose.connect(
   // 'mongodb://localhost:27017/course-goals',
   // 'mongodb://host.docker.internal:27017/course-goals',
-  'mongodb://heath:secret@mongodb:27017/course-goals?authSource=admin',
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/course-goals?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
   (err) => {
     if (err) {
-      console.error('FAILED TO CONNECT TO MONGODB');
+      console.error('FAILED TO CONNECT TO MONGODB!!');
       console.error(err);
     } else {
-      console.log('CONNECTED TO MONGODB');
+      console.log('CONNECTED TO MONGODB!!');
       app.listen(80);
     }
   }
