22b2f67de2488b251c40760a7cdc3a3709ab1f26
heathchang <jannhyunsoo@gmail.com>
2022-10-07 20:53:23 +0900

    react_pokemon

---
+++ Project/05_pokemon/src/App.js
@@ -0,0 +1,15 @@
+import React, { Suspense } from "react";
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+
+// const _ = React.lazy(() => import(''))
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!!!!</h2>
+    </div>
+  );
+}
+
+export default App;
