22a64e60024359d4a3a532030f8b8f9a862c4728
heathchang <jannhyunsoo@gmail.com>
2022-11-11 08:20:01 +0900

    intro customHooks

--- React/newCompleteGuide/14.sendingHTTPRequest/src/App.js
+++ React/newCompleteGuide/14.sendingHTTPRequest/src/App.js
@@ -1,15 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
     const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    const fetchMoviesHandler = async () => {
-        const response = await fetch('https://swapi.dev/api/films')
 
-        if (response.status === 200) {
+    const fetchMoviesHandler = useCallback(async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch('https://swapi.dev/api/films')
+            if (response.status !== 200) {
+                throw new Error('Error Occurred');
+            }
             const movies = await response.json();
             const movieData = movies.results.map(item => {
                 return {
@@ -20,8 +27,18 @@ function App() {
                 }
             })
             setMovies(movieData);
+        } catch (e) {
+            setError(e.message);
+        } finally {
+            console.log('finally')
+            setIsLoading(false)
         }
-    }
+    }, [])
+
+
+    useEffect(() => {
+        fetchMoviesHandler()
+    }, [fetchMoviesHandler]);
 
     return (
         <React.Fragment>
@@ -29,7 +46,10 @@ function App() {
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             </section>
             <section>
-                <MoviesList movies={movies}/>
+                {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+                {!isLoading && movies.length === 0 && !error && <p>Found No Movies</p>}
+                {!isLoading && error && <p> {error}</p>}
+                {isLoading && <p>Loading</p>}
             </section>
         </React.Fragment>
     );
