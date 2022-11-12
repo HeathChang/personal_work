0cda4ea99c5ed90808ba69bd35b17666a9431e06
heathchang <jannhyunsoo@gmail.com>
2022-11-12 17:32:35 +0900

    endCustome_startRedux

---
+++ React/newCompleteGuide/15a.customHooks_sect2/src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+
+    //fn: transformTask:::::
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: 'https://react-http-6b4a6.firebaseio.com/tasks.json' },
+      transformTasks
+    );
+
+  }, [fetchTasks]);
+  // fetchTasks가 변경될때마다 실행
+
+
+  //Event Handler::
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
