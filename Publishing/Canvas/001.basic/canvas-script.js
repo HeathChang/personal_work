4fda78d44a8f1e8e92a0c0932a94f50fac9ff95a
heathchang_desktop <jannhyunsoo@gmail.com>
2023-03-22 22:18:43 +0900

    basesetyp-1

--- Publishing/Canvas/001.basic/canvas-script.js
+++ Publishing/Canvas/001.basic/canvas-script.js
@@ -1,9 +1,19 @@
-const canvas = document.getElementById("hello-world-canvas");
-const context = canvas.getContext("2d");
+// const canvas = document.getElementById("hello-world-canvas");
+// const context = canvas.getContext("2d"); // 스크립트가 랜더링 컨텍스트에 접근
 
 // Blue Rect
-context.fillStyle= "blue";
-context.fillRect(10,40,30,70);
+// context.fillStyle= "blue";
+// context.fillRect(10,40,30,70);
+//
+// context.fillStyle = "yellow"
+// context.fillRect(50,30,60,30);
 
-context.fillStyle = "yellow"
-context.fillRect(50,30,60,30);
+window.onload = () => {
+    const canvas = document.getElementById("hello-world-canvas");
+    const context = canvas.getContext("2d"); // 스크립트가 랜더링 컨텍스트에 접근
+
+    context.beginPath(); //reset the context state.
+    context.moveTo(30, 70) // starting point  of line.
+    context.lineTo(130, 70) // end point of the line.
+    context.stroke();
+}
