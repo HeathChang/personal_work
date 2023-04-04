63c59bf67ed5333aefa4f2c1e91ed050dd886b9f
heathchang <jannhyunsoo@gmail.com>
2023-04-04 23:48:03 +0900

    dart basic_1

---
+++ Flutter/Flutter_Max/flutter02_first_app/lib/files/basicDart.dart
@@ -0,0 +1,24 @@
+void main() {
+  // 1. basic
+  var firstResult = addNumbers(1, 2);
+  print(firstResult);
+
+  // 2. Object is class in Dart
+  var person = Person(); // in Dart, new is not needed
+  var newName = person.name;
+  var newAge = person.age;
+  print(newName);
+  print(newAge);
+
+
+}
+
+int addNumbers(int num1, int num2) {
+  return num1 + num2;
+}
+
+
+class Person {
+  String name = 'Heath';
+  int age = 30;
+}
