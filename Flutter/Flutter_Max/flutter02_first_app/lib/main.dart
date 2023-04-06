eaedcc625c5f87c4f59edf288fa6c0c9f048c7cf
heathchang <jannhyunsoo@gmail.com>
2023-04-07 00:15:22 +0900

    hello world

--- Flutter/Flutter_Max/flutter02_first_app/lib/main.dart
+++ Flutter/Flutter_Max/flutter02_first_app/lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
-// void main() {
-//   //create an Object.
-//   runApp(MyApp());
-// }
-
-void main() => {
+void main() {
+  //create an Object.
   runApp(MyApp());
-};
+}
 
 class MyApp extends StatelessWidget {
   // when extends, MyApp will cause error => add Method
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello Flutter"),); //main widget. every widget is  class
+        // home: Text("Hello Flutter"),); //main widget. every widget is  class
+        home: Scaffold(
+      appBar: AppBar(title: Text("My First App")),
+      body: Text('This is my default text'),
+      bottomNavigationBar: Text('Hello World'),
+    ));
   }
 }
