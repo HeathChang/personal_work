f35c9e29250fd31e99f2b6600517fb63b35fbed9
heathchang <jannhyunsoo@gmail.com>
2023-04-26 20:42:15 +0900

    quiz end

---
+++ Flutter/Flutter_Max/flutter04_quiz/lib/components/questions_summary.dart
@@ -0,0 +1,54 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          // Maps returns a Iterable which cannot assigned to Widget
+          // Needs to mutate List by using toList();
+          children: summaryData.map((data) {
+            return Row(children: [
+              Text(((data['question_index'] as int) + 1).toString()),
+              const SizedBox(width: 20, height: 100),
+
+              // use Type-Casting
+              Expanded(
+                // Expanded widgets allow it child to take the available space along the flex widgets main axis.
+                // Flex Widget: Row or Column widget that i put into expand
+                // THis restricted to the width of a row.
+                child: Column(
+                  children: [
+                    Text(data['question'] as String),
+                    const SizedBox(height: 5),
+                    Row(
+                      children: [
+                        const Text('Your: '),
+                        Text(data['user_answer'] as String),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Text('Correct: '),
+                        Text(data['correct_answer'] as String),
+                      ],
+                    ),
+                    const SizedBox(height: 30,)
+                  ],
+                ),
+              ),
+            ]);
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
--- Flutter/Flutter_Max/flutter04_quiz/lib/components/quiz.dart
+++ Flutter/Flutter_Max/flutter04_quiz/lib/components/quiz.dart
@@ -1,7 +1,11 @@
 // quiz.dart contains contents MaterialApp
+import 'package:flutter/material.dart';
+import 'package:basic101_01/components/data/questions.dart';
 import 'package:basic101_01/components/questions_screen.dart';
 import 'package:basic101_01/components/start_screen.dart';
-import 'package:flutter/material.dart';
+import 'package:basic101_01/components/results_screen.dart';
+
+
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -13,47 +17,39 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  // 방법 1.
-  // Widget? activeScreen; // able to store widgets in var
-  // // error because using switchScreen when initializing the activeScreen variable at the same point of time.
-  //
-  // @override
-  // void initState() {
-  //   // initState will execute once after the object has been created
-  //   // this does not excute at all at this time, so no need to use setState
-  //   activeScreen = StartScreen(switchScreen);
-  //   super.initState();
-  // }
-  //
-  // void switchScreen() {
-  //   setState(() {
-  //     //QuestionScreen should follow StartScreen();
-  //     activeScreen = const QuestionScreen();
-  //   });
-  // }
-
-  // 방법 2.
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      //QuestionScreen should follow StartScreen();
       activeScreen = 'questions-screen';
     });
   }
 
-  final List<String> selectedAnswers = [];
-
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     Widget screenWidget = StartScreen(switchScreen);
 
     if (activeScreen == 'questions-screen') {
-      screenWidget = QuestionScreen(chooseAnswer);
+      screenWidget = QuestionScreen(
+        chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
     }
 
     return MaterialApp(
@@ -69,12 +65,104 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          // 방법 1
-          // child: activeScreen,
-          // 방법 2: ternary expression
           child: screenWidget,
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+// class Quiz extends StatefulWidget {
+//   const Quiz({super.key});
+//
+//   @override
+//   State<Quiz> createState() {
+//     return _QuizState();
+//   }
+// }
+
+// class _QuizState extends State<Quiz> {
+//   // 방법 1.
+//   // Widget? activeScreen; // able to store widgets in var
+//   // // error because using switchScreen when initializing the activeScreen variable at the same point of time.
+//   //
+//   // @override
+//   // void initState() {
+//   //   // initState will execute once after the object has been created
+//   //   // this does not excute at all at this time, so no need to use setState
+//   //   activeScreen = StartScreen(switchScreen);
+//   //   super.initState();
+//   // }
+//   //
+//   // void switchScreen() {
+//   //   setState(() {
+//   //     //QuestionScreen should follow StartScreen();
+//   //     activeScreen = const QuestionScreen();
+//   //   });
+//   // }
+//
+//   // 방법 2.
+//   var activeScreen = 'start-screen';
+//
+//   void switchScreen() {
+//     setState(() {
+//       activeScreen = 'questions-screen';
+//     });
+//   }
+//
+//   List<String> selectedAnswers = [];
+//
+//   void chooseAnswer(String answer) {
+//     selectedAnswers.add(answer);
+//
+//     if (selectedAnswers.length == questions.length) {
+//       setState(() {
+//         selectedAnswers = [];
+//         activeScreen = 'results-screen';
+//       });
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     Widget screenWidget = StartScreen(switchScreen);
+//
+//     if (activeScreen == 'questions-screen') {
+//       screenWidget = QuestionScreen(
+//          chooseAnswer,
+//       );
+//     }
+//
+//     if (activeScreen == 'results-screen') {
+//       screenWidget = ResultsScreen(
+//         chosenAnswers: selectedAnswers,
+//       );
+//
+//
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Container(
+//           decoration: const BoxDecoration(
+//             gradient: LinearGradient(
+//               colors: [
+//                 Color.fromARGB(255, 78, 13, 151),
+//                 Color.fromARGB(255, 107, 15, 168),
+//               ],
+//               begin: Alignment.topLeft,
+//               end: Alignment.bottomRight,
+//             ),
+//           ),
+//           // 방법 1
+//           // child: activeScreen,
+//           // 방법 2: ternary expression
+//           child: screenWidget,
+//         ),
+//       ),
+//     );
+//   }
+// }
