4a55ac2d71f2aca9440314221dbe087205bd38e8
heathchang <jannhyunsoo@gmail.com>
2023-05-26 16:48:21 +0900

    swift

---
+++ Swift/swift01_Restart/Restart-App/Students/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Hyunsoo Chang on 2023/05/26.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift/swift01_Restart/Restart-App/Students/Restart/Restart/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Hyunsoo Chang on 2023/05/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, Home View!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
