db70cf7fe57a85e231aba4e1da5906dc02758f0c
heathchang <jannhyunsoo@gmail.com>
2023-06-01 16:38:15 +0900

    test

---
+++ Swift/IOS16/11111/11111/_1111App.swift
@@ -0,0 +1,17 @@
+//
+//  _1111App.swift
+//  11111
+//
+//  Created by Hyunsoo Chang on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct _1111App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
