b79f3740d295f4c6d90fc2b246bbffc114ea0126
heathchang <jannhyunsoo@gmail.com>
2023-06-02 16:46:44 +0900

    location ing

--- Flutter/Flutter_Max/flutter10_device/lib/screens/place_detail.dart
+++ Flutter/Flutter_Max/flutter10_device/lib/screens/place_detail.dart
@@ -10,17 +10,23 @@ class PlaceDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(place.title),
-      ),
-      body: Center(
-        child: Text(
-          place.title,
-          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
+        appBar: AppBar(
+          title: Text(place.title),
         ),
-      ),
-    );
+        // Stack: allows to display multiple other widgets on top each other
+        // Stack allows you to stack child widget on top each other (can be overlapped)
+        // Column widget arranges child widgets vertically. (no overlapped)
+
+
+        body: Stack(
+          children: [
+            Image.file(
+              place.image,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            )
+          ],
+        ));
   }
 }
