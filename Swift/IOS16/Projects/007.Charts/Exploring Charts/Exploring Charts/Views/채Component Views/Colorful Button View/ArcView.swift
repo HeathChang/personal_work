a0296d4864f720e6e85ed6d4770aec8aba76da72
heathchang <jannhyunsoo@gmail.com>
2023-06-16 21:42:01 +0900

    chart-setup-ing

--- Swift/IOS16/Projects/007.Charts/Exploring Charts/Exploring Charts/Views/채Component Views/Colorful Button View/ArcView.swift
+++ Swift/IOS16/Projects/007.Charts/Exploring Charts/Exploring Charts/Views/채Component Views/Colorful Button View/ArcView.swift
@@ -7,14 +7,33 @@ struct ArcView: View {
     let startTrim: CGFloat;
     let endTrim: CGFloat;
     let rotate: CGFloat;
-    @State private var finalTrime: CGFloat = 0;
+    @State private var finalTrim: CGFloat = 0;
+    
+    
     var lineWidth: CGFloat {
         endRadius - startRadius
     }
     
+    var finalRadius: CGFloat {
+        endRadius-lineWidth
+    }
+    
     var body: some View {
         Circle()
-            .frame()
+            .trim(from: startTrim, to: finalTrim)
+            .stroke(color,
+                    style: StrokeStyle(
+                        lineWidth: lineWidth,
+                        lineCap: .round)
+            )
+            .rotationEffect(.degrees(rotate))
+            .frame(width: finalRadius,
+                   height: endRadius)
+            .onAppear{
+                withAnimation{
+                    finalTrim = endTrim
+                }
+            }
     }
 }
 
@@ -22,11 +41,13 @@ struct ArcView_Previews: PreviewProvider {
     static var previews: some View {
         ArcView(
             color: Color.darkOrchid,
-            startRadius: 90,
-            endRadius: 100,
+            startRadius: 170,
+            endRadius: 190,
             startTrim: 0.25,
             endTrim: 0.75,
             rotate: 30
         )
+        Circle()
+            .frame(width: 100, height: 100)
     }
 }
--- Swift/IOS16/Projects/007.Charts/Exploring Charts/Exploring Charts/Views/채Component Views/Colorful Button View/RandomArcFromColorListView.swift
+++ Swift/IOS16/Projects/007.Charts/Exploring Charts/Exploring Charts/Views/채Component Views/Colorful Button View/RandomArcFromColorListView.swift
@@ -1,20 +1,57 @@
-//
-//  RandomArcFromColorListView.swift
-//  Exploring Charts
-//
-//  Created by Hyunsoo Chang on 2023/06/15.
-//
 
 import SwiftUI
 
 struct RandomArcFromColorListView: View {
+    // Params
+    let colors: [Color];
+    let index: Int;
+    let minRadius: CGFloat;
+    let maxRadius: CGFloat;
+    let opacity: CGFloat;
+    
+    // Const
+    let startTrim = Double.random(in: 0...0.5)
+    let endTrim = 1.0
+    
+    // computed property
+    var isValidIndex: Bool {
+        return index < colors.count && index >= 0
+    }
+    var theIndex: Int{
+        return isValidIndex ? index : 0
+    }
+    var count: CGFloat {
+        return CGFloat(colors.count)
+    }
+    var color: Color{
+        colors[theIndex]
+    }
+    var endRadius: CGFloat{
+        minRadius + maxRadius / count
+    }
+    var rotate: CGFloat {
+        Double.random(in: 0...360)
+    }
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ArcView(color: color.opacity(opacity),
+                startRadius: minRadius,
+                endRadius: endRadius,
+                startTrim: startTrim,
+                endTrim: 1,
+                rotate: rotate)
     }
 }
 
 struct RandomArcFromColorListView_Previews: PreviewProvider {
     static var previews: some View {
-        RandomArcFromColorListView()
+        RandomArcFromColorListView(
+            colors: Color.defaultColors,
+            index: 2,
+            minRadius: 170,
+            maxRadius: 180,
+            opacity: 0.9
+        )
     }
 }
