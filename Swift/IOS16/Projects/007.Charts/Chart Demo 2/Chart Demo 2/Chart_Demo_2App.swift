4b292ce3dca884fce3de996aef48f7edc6a02b2a
heathchang <jannhyunsoo@gmail.com>
2023-06-21 21:51:42 +0900

    Swift-Chart-Final-Setup2

--- Swift/IOS16/Projects/007.Charts/Chart Demo 2/Chart Demo 2/Chart_Demo_2App.swift
+++ Swift/IOS16/Projects/007.Charts/Chart Demo 2/Chart Demo 2/Chart_Demo_2App.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct Chart_Demo_2App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
-        }
+            ContentView(
+                dailySales: defaultDailySales,
+                min: 0.0,
+                max: 700.0
+            )        }
     }
 }
--- Swift/IOS16/Projects/007.Charts/Chart Demo 2/Chart Demo 2/ContentView.swift
+++ Swift/IOS16/Projects/007.Charts/Chart Demo 2/Chart Demo 2/ContentView.swift
@@ -1,19 +1,93 @@
-//
-//  ContentView.swift
-//  Chart Demo 2
-//
-//  Created by Hyunsoo Chang on 2023/06/21.
-//
 
 import SwiftUI
+import Charts
+
+enum ChartType {
+    case bar, line, area
+}
 
 struct ContentView: View {
+    let dailySales: [DailySalesType]
+    let min: Double
+    let max: Double
+    @State var barColors: [Color] = defaultBarColors
+    @State var chartType: ChartType = .bar
+    let xAxisMarkPosition: AxisMarkPosition = .bottom
+    let yAxisMarkPosition: AxisMarkPosition = .leading
+    @State private var isVerticalChart = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Chart Demo 3")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            if isVerticalChart {
+                switch(chartType) {
+                case .bar:
+                    BarChartVerticalView(dailySales: dailySales, barColors: barColors)
+                case .line:
+                    LineChartVerticalView(dailySales: dailySales)
+                case .area:
+                    AreaChartVerticalView(dailySales: dailySales)
+                }
+            } else {
+                switch(chartType) {
+                case .bar:
+                    BarChartHorizontalView(dailySales: dailySales, barColors: barColors)
+                case .line:
+                    LineChartHorizontalView(dailySales: dailySales)
+                case .area:
+                    AreaChartHorizontalView(dailySales: dailySales)
+                }
+            }
+            
+            // Chart Buttons
+            HStack {
+                
+                ColorfulButtonView(
+                    colors: $barColors,
+                    dim: 30,
+                    offset: 10,
+                    action: {})
+                
+                Spacer()
+                
+                Button(action: {
+                    withAnimation {
+                        chartType = .bar
+                    }
+                }, label: {
+                    Text("BAR")
+                })
+                Spacer()
+                Button(action: {
+                    withAnimation {
+                        chartType = .line
+                    }
+                }, label: {
+                    Text("LINE")
+                })
+                Spacer()
+                Button(action: {
+                    withAnimation {
+                        chartType = .area
+                    }
+                }, label: {
+                    Text("AREA")
+                })
+                Spacer()
+                Button(action: {
+                    withAnimation {
+                        isVerticalChart.toggle()
+                    }
+                }, label: {
+                    Image(systemName: "chart.bar.fill")
+                        .rotationEffect(.degrees(isVerticalChart ? 90 : 0))
+                })
+                
+            }
+            .padding()
         }
         .padding()
     }
@@ -21,6 +95,10 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            dailySales: defaultDailySales,
+            min: 0.0,
+            max: 700.0)
     }
 }
+
