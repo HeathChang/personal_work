3119eb79262d37c2d620358c2b2c66f0da70ddb5
heathchang <jannhyunsoo@gmail.com>
2023-07-13 01:03:30 +0900

    swift-alarm-sound3

--- Swift/IOS16/Projects/008.Alrams/Alarm/Alarm/Views/Sub View/ToBedWakeUpView.swift
+++ Swift/IOS16/Projects/008.Alrams/Alarm/Alarm/Views/Sub View/ToBedWakeUpView.swift
@@ -7,118 +7,77 @@ struct ToBedWakeUpView: View {
     
     
     var body: some View {
-        VStack {
-            // Cancel and Save Button
-            CancelSaveAlarm(currentAlarmIndex: currentIndex, alarmModel: $alarmModel)
-            
-            /// Alarm Toggler
-            AlarmToggleView(alarmEnabled: $alarmModel.alarmEnabled)
-            
-            Divider()
-            
-            HStack{
-                VStack {
-                    Grid{
-                        GridRow{
-                            TimeOfDayIcon(date: alarmModel.start)
-                                .font(.largeTitle)
-                            VStack(alignment: .leading){
-                                GrayedTextView(text: "Start")
-                                TimePicker(time: $alarmModel.start, scale: 1.3)
+        NavigationStack {
+            VStack {
+                // Cancel and Save Button
+                CancelSaveAlarm(currentAlarmIndex: currentIndex, alarmModel: $alarmModel)
+                
+                /// Alarm Toggler
+                AlarmToggleView(alarmEnabled: $alarmModel.alarmEnabled)
+                
+                Divider()
+                
+                HStack{
+                    VStack {
+                        Grid{
+                            GridRow{
+                                TimeOfDayIcon(date: alarmModel.start)
+                                    .font(.largeTitle)
+                                VStack(alignment: .leading){
+                                    GrayedTextView(text: "Start")
+                                    TimePicker(time: $alarmModel.start, scale: 1.3)
+                                }
                             }
-                        }
-                        GridRow{
-                            HStack{
-                                Divider()
-                                    .frame(height: 30)
-                                    .padding(2)
-                            }
-                        }
-                        GridRow{
-                            Image(systemName: alarmModel.activity)
-                                .foregroundColor(alarmModel.activityColor)
-                                .font(.headline)
-                            SelectActivityView(currentColorIndex: $alarmModel.colorIndex,
-                                               currentActivity:  $alarmModel.activity)
-                        }.padding(.vertical)
-                        GridRow{
-                            HStack{
-                                Divider()
-                                    .frame(height: 30)
-                                    .padding(2)
-                            }
-                        }
-                        GridRow{
-                            TimeOfDayIcon(date: alarmModel.end)
-                                .font(.largeTitle)
-                            VStack(alignment: .leading){
-                                TimePicker(time: $alarmModel.end, scale: 1.3)
-                                
-                                GrayedTextView(text: "End")
-                            }
-                        } 
-                        GridRow{
-                            Text("")
-//                            SoundMenuView(alarmModel: $alarmModel)
-                            Menu(content: {
-                                //pop-up menu that appears when the user performs a long press or right-click on the view. The menu options defined within the curly braces { ... } will be displayed in the context menu
-                                ForEach(Sounds.allCases, id: \.self){ sound in
-                                    Button(action: {
-                                        alarmModel.sound = sound
-                                    }, label: {
-                                        Text(sound.rawValue)
-                                    })
-                                }.padding(.vertical)
-                            }, label: {
+                            GridRow{
                                 HStack{
-                                    Text("Sound")
-                                        .fontWeight(.semibold)
+                                    Divider()
+                                        .frame(height: 30)
+                                        .padding(2)
+                                }
+                            }
+                            GridRow{
+                                Image(systemName: alarmModel.activity)
+                                    .foregroundColor(alarmModel.activityColor)
+                                    .font(.headline)
+                                SelectActivityView(currentColorIndex: $alarmModel.colorIndex,
+                                                   currentActivity:  $alarmModel.activity)
+                            }.padding(.vertical)
+                            GridRow{
+                                HStack{
+                                    Divider()
+                                        .frame(height: 30)
+                                        .padding(2)
+                                }
+                            }
+                            GridRow{
+                                TimeOfDayIcon(date: alarmModel.end)
+                                    .font(.largeTitle)
+                                VStack(alignment: .leading){
+                                    TimePicker(time: $alarmModel.end, scale: 1.3)
                                     
-                                    Text(alarmModel.sound.rawValue)
-                                        .font(.caption)
-                                        .fontWeight(.thin)
-                                    
-                                }.padding(7)
-                                    .overlay( //  to create a layer on top of another element
-                                        Capsule() // rounded rectangle with semi-circular ends, resembling a pill or capsule shape
-                                            .stroke()
-                                    ) // rendered using the default stroke color and width
-                            })
-                            //                            HStack{
-                            //                                Text("Sound")
-                            //                                    .fontWeight(.semibold)
-                            //
-                            //                                Text(alarmModel.sound.rawValue)
-                            //                                    .font(.caption)
-                            //                                    .fontWeight(.thin)
-                            //
-                            //                            }.padding(7)
-                            //                                .overlay( //  to create a layer on top of another element
-                            //                                    Capsule() // rounded rectangle with semi-circular ends, resembling a pill or capsule shape
-                            //                                        .stroke()
-                            //                                ) // rendered using the default stroke color and width
-                            //                                .contextMenu{
-                            //                                    //pop-up menu that appears when the user performs a long press or right-click on the view. The menu options defined within the curly braces { ... } will be displayed in the context menu
-                            //                                    ForEach(Sounds.allCases, id: \.self){ sound in
-                            //                                        Button(action: {
-                            //                                            alarmModel.sound = sound
-                            //                                        }, label: {
-                            //                                            Text(sound.rawValue)
-                            //                                        })
-                            //                                    }.padding(.vertical)
-                            //                                }
-                        }
-                    }.frame(maxWidth: .infinity, alignment: .leading)
-                    
-                }.padding()
-            }
-            
-        }.padding().background(cardBackgroundColor.cornerRadius(20)).padding()
+                                    GrayedTextView(text: "End")
+                                }
+                            }
+                            GridRow{
+                                Text("")
+                                SoundMenuViewFromButton(alarmModel: $alarmModel)
+                                
+                            }
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                        
+                    }.padding()
+                }
+                
+            }.padding().background(cardBackgroundColor.cornerRadius(20)).padding()
+        }
     }
 }
 
 struct ToBedWakeUpView_Previews: PreviewProvider {
     static var previews: some View {
-        ToBedWakeUpView(currentIndex: nil, alarmModel: AlarmModel.DefaultAlaram())
+        ZStack{
+            Color.gray.opacity(0.9).ignoresSafeArea()
+            ToBedWakeUpView(currentIndex: nil, alarmModel: AlarmModel.DefaultAlaram())
+        }
     }
 }
--- Swift/IOS16/Projects/008.Alrams/Alarm/Alarm/Views/Supporting View/SoundMenuView.swift
+++ Swift/IOS16/Projects/008.Alrams/Alarm/Alarm/Views/Supporting View/SoundMenuView.swift
@@ -11,7 +11,8 @@ struct SoundMenuView: View {
                         // change current sound
                         alarmSound = sound
                         playSound(sound: sound.rawValue)
-                    }, label: {
+                    },
+                           label: {
                         HStack {
                             Image(systemName:  "checkmark" )
                                 .foregroundColor(purple)
@@ -26,15 +27,62 @@ struct SoundMenuView: View {
             }
             Section(header: Text("Sounds of Nature")) {
                 ForEach(natureSoundsList, id: \.self){ sound in
-                    Text("Hello, World!")
+                    Button(action: {
+                        // change current sound
+                        alarmSound = sound
+                        playSound(sound: sound.rawValue)
+                    },
+                           label: {
+                        HStack {
+                            Image(systemName:  "checkmark" )
+                                .foregroundColor(purple)
+                                .fontWeight(.semibold)
+                                .opacity(alarmSound == sound ? 1.0 : 0.0)
+                            Text(sound.rawValue.formatSoundName)
+                                .foregroundColor(black)
+                                .fontWeight(.semibold)
+                        }
+                    })
                 }
             }
         }
+        .onDisappear{
+            // Stop play music
+            stopPlaying()
+        }
+    }
+}
+
+
+struct SoundMenuViewFromButton: View {
+    @Binding var alarmModel: AlarmModel
+    var body: some View {
+        NavigationLink(
+            destination: {
+                SoundMenuView(alarmSound: $alarmModel.sound)
+            }, label: {
+                HStack{
+                    Text("Sound")
+                        .fontWeight(.semibold)
+                    Text(alarmModel.sound.rawValue)
+                        .font(.caption)
+                        .fontWeight(.thin)
+                }.padding(7)
+                    .overlay(
+                        Capsule()
+                            .stroke()
+                    )
+            })
     }
 }
 
 struct SoundMenuView_Previews: PreviewProvider {
     static var previews: some View {
-        SoundMenuView(alarmSound: .constant(AlarmModel.DefaultAlaram().sound))
+        ZStack{
+            NavigationStack{
+                SoundMenuView(alarmSound: .constant(AlarmModel.DefaultAlaram().sound))
+                SoundMenuViewFromButton(alarmModel: .constant(AlarmModel.DefaultAlaram()))
+            }
+        }
     }
 }
