adeca26df9406c987327727c25ecac7eb06dd20e
heathchang <jannhyunsoo@gmail.com>
2023-08-10 23:39:46 +0900

    swift-fastcampus

---
+++ Swift/IOS16/Projects2/01.AssetManagement/AssetManagement/AssetManagement/AssetManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetManagementApp.swift
+//  AssetManagement
+//
+//  Created by Hyunsoo Chang on 2023/08/10.
+//
+
+import SwiftUI
+
+@main
+struct AssetManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift/IOS16/Projects2/01.AssetManagement/AssetManagement/AssetManagement/ContentView.swift
@@ -0,0 +1,60 @@
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection: Tab = Tab.asset
+    
+    enum Tab {
+        case asset
+        case recommend
+        case alarm
+        case setting
+    }
+    // ios 15부터, tab-bar 구분선 추가해줘야함.
+    init() {
+        UITabBar.appearance().scrollEdgeAppearance = .init()
+    }
+    
+    var body: some View {
+        // 하나의 화면에 여러 개의 View를 Tab 방식
+        TabView(selection: $selection){
+            //  it's a two-way connection between the TabView and the variable selection in your code.
+            Color.white
+                .tabItem {
+                    Image(systemName: "dollarsign.circle.fill")
+                    Text("자산")
+                }
+                .tag(Tab.asset)
+            Color.blue
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "hand.thumbsup.fill")
+                    Text("추천")
+                }
+                .tag(Tab.recommend)
+            Color.yellow
+                .badge(10)
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "bell.fill")
+                    Text("알림")
+                }
+                .tag(Tab.alarm)
+            Color.red
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("설정")
+                }
+                .tag(Tab.setting)
+            
+        }
+        .preferredColorScheme(.light)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
