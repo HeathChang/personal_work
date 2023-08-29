0d9aae0dd20f807aa0c23bb65381a808f18518b9
heathchang <jannhyunsoo@gmail.com>
2023-08-29 21:54:18 +0900

    swift-chap8-pinch-setup

---
+++ Swift/IOS16/Project_with_AI/Swift02_Pinch/Students/Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Hyunsoo Chang on 2023/08/29.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
