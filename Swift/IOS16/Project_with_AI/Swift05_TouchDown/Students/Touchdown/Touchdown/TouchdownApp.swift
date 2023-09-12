d2124a7e013eeb9b6062440f412156c1173fbeff
heathchang <jannhyunsoo@gmail.com>
2023-09-12 15:47:22 +0900

    swift-chap11-TouchDown-setup2

---
+++ Swift/IOS16/Project_with_AI/Swift05_TouchDown/Students/Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Hyunsoo Chang on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift/IOS16/Project_with_AI/Swift06_Devote/Students/Devote/Devote/DevoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Hyunsoo Chang on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
