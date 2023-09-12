9070d9f61e988246fac92ecd191822c3fa9aa475
heathchang <jannhyunsoo@gmail.com>
2023-09-12 09:27:30 +0900

    swift-chap11-TouchDown-setup

---
+++ Swift/IOS16/Project_with_AI/Swift05_TouchDown/Students/TouchDown/TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Hyunsoo Chang on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
