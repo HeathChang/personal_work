f01e59131b0d721497f7c6d49ec22af47e313be8
heathchang <jannhyunsoo@gmail.com>
2023-09-15 14:11:33 +0900

    swift-chap12-Devote-2setup

--- Swift/IOS16/Project_with_AI/Swift06_Devote/Students/Devote/Devote/DevoteApp.swift
+++ Swift/IOS16/Project_with_AI/Swift06_Devote/Students/Devote/Devote/DevoteApp.swift
@@ -2,16 +2,19 @@
 //  DevoteApp.swift
 //  Devote
 //
-//  Created by Hyunsoo Chang on 2023/09/12.
+//  Created by Hyunsoo Chang on 2023/09/15.
 //
 
 import SwiftUI
 
 @main
 struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
