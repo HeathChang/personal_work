2dd3d9ce1a94df4fb18f19f96f40b8d6763a3d0c
heathchang <jannhyunsoo@gmail.com>
2023-09-15 13:04:58 +0900

    swift-chap12-Devote

---
+++ Swift/IOS16/Project_with_AI/Swift06_Devote/Students/Devote/Devote/Model/Persistence.swift
@@ -0,0 +1,14 @@
+
+import CoreData
+
+struct PersistenceController {
+  // MARK: - 1. PERSISTENT CONTROLLER
+  
+  // MARK: - 2. PERSISTENT CONTAINER
+  
+  // MARK: - 3. INITIALIZATION (load the persistent store)
+
+  
+  // MARK: - 4. PREVIEW
+
+}
